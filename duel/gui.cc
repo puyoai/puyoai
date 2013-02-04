4d883f8a399aecb7eccfd84e50a62a1696a951bd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2013-02-04 11:51:31 +0900

    Add Commentator for duel

--- duel/gui.cc
+++ duel/gui.cc
@@ -6,13 +6,18 @@
 #include <stdlib.h>
 
 #include <iomanip>
+#include <memory>
 #include <sstream>
 
 #include <SDL.h>
 
 #include <glog/logging.h>
 
+#include <core/field.h>
+#include <core/state.h>
+
 #include "SDL_kanji.h"
+#include "commentator.h"
 #include "field_realtime.h"
 #include "screen.h"
 
@@ -21,6 +26,12 @@ using namespace std;
 const char* kJapaneseBdfName = "data/jiskan24.bdf";
 const char* kEnglishBdfName = "data/12x24.bdf";
 const int kBdfSize = 24;
+
+static const int PUYO_W = 20;
+static const int PUYO_H = 20;
+static const int PUYO_OFF_X = 1;
+static const int PUYO_OFF_Y = 1;
+static const int PUYO_GAME_W = 13;
 
 class GuiSdl : public Gui {
 public:
@@ -38,6 +49,27 @@ public:
       b.dy = 424;
       scr_ = new Screen(scr);
       scr_->setMainBox(512, 384, b);
+
+      commentator_.reset(new Commentator());
+      for (int i = 0; i <= GREEN; i++) {
+        commentator_->setColorMap(i, i);
+      }
+
+      for (int pi = 0; pi < 2; pi++) {
+        int pos_x = PUYO_OFF_X + PUYO_GAME_W * pi;
+        int pos_y = PUYO_OFF_Y;
+        for (int y = 0; y < Field::HEIGHT; y++) {
+          for (int x = 0; x < Field::MAP_WIDTH; x++) {
+            Screen::Box b;
+            b.sx = (pos_x + 1 + x) * PUYO_W;
+            b.sy = (pos_y - 1 + Field::MAP_HEIGHT - y) * PUYO_H;
+            b.dx = b.sx + PUYO_W;
+            b.dy = b.sy + PUYO_H;
+            bb_[pi][x][11-y] = b;
+          }
+        }
+      }
+      scr_->setBoundingBox(&bb_[0][0][0]);
     } else {
       SDL_Surface* scr = SDL_SetVideoMode(512, 384, 32,
                                           SDL_SWSURFACE | SDL_DOUBLEBUF);
@@ -59,20 +91,27 @@ public:
 
   virtual void Draw(const FieldRealtime& field,
                     const string& debug_message) {
-#ifdef USE_SDL
+    if (commentator_.get()) {
+      commentator_->setAIMessage(field.player_id(), debug_message);
+      if ((field.GetStateInfo() & STATE_YOU_GROUNDED) != 0) {
+        Field f(field.GetFieldInfo());
+        commentator_->setField(field.player_id(), f, true);
+      }
+    }
+
     int x1, y1, x2, y2, r;
     char c1, c2;
     field.GetCurrentPuyo(&x1, &y1, &c1, &x2, &y2, &c2, &r);
 
-    int pos_x = 1 + 13 * field.player_id();
-    int pos_y = 1;
+    int pos_x = PUYO_OFF_X + PUYO_GAME_W * field.player_id();
+    int pos_y = PUYO_OFF_Y;
     for (int y = 0; y < Field::MAP_HEIGHT; y++) {
       for (int x = 0; x < Field::MAP_WIDTH; x++) {
         SDL_Rect r;
-        r.x = (pos_x + x) * 20;
-        r.y = (pos_y + Field::MAP_HEIGHT - y) * 20;
-        r.w = 20;
-        r.h = 20;
+        r.x = (pos_x + x) * PUYO_W;
+        r.y = (pos_y + Field::MAP_HEIGHT - y) * PUYO_H;
+        r.w = PUYO_W;
+        r.h = PUYO_H;
 
         char color = field.Get(x, y);
         if (field.IsInUserState()) {
@@ -92,9 +131,9 @@ public:
     // Death line
     {
       SDL_Rect r;
-      r.x = pos_x * 20;
-      r.y = (pos_y + 4) * 20;
-      r.w = 20 * 8;
+      r.x = pos_x * PUYO_W;
+      r.y = (pos_y + 4) * PUYO_H;
+      r.w = PUYO_H * 8;
       r.h = 1;
       SDL_FillRect(scr_->off(), &r,
                    SDL_MapRGB(scr_->off()->format, 255, 255, 255));
@@ -103,10 +142,10 @@ public:
     // Next puyo info
     for (int i = 2; i < 6; i++) {
       SDL_Rect r;
-      r.x = (pos_x + 9) * 20;
-      r.y = (pos_y + + 3 + (i - 2) + ((i - 2) / 2)) * 20;
-      r.w = 20;
-      r.h = 20;
+      r.x = (pos_x + 9) * PUYO_W;
+      r.y = (pos_y + + 3 + (i - 2) + ((i - 2) / 2)) * PUYO_H;
+      r.w = PUYO_W;
+      r.h = PUYO_H;
       Uint32 c = GetPuyoColor(field.GetNextPuyo(i));
       SDL_FillRect(scr_->off(), &r, c);
     }
@@ -119,38 +158,39 @@ public:
     {
       ostringstream ss;
       ss << setw(10) << field.GetScore();
-      Kanji_PutText(font_, pos_x * 20, (pos_y + Field::MAP_HEIGHT + 1) * 20,
+      Kanji_PutText(font_,
+                    pos_x * PUYO_W,
+                    (pos_y + Field::MAP_HEIGHT + 1) * PUYO_H,
+                    scr_->off(), ss.str().c_str(), white);
+    }
+
+    // Ojama
+    {
+      ostringstream ss;
+      ss << field.GetFixedOjama() << '(' << field.GetPendingOjama() << ')';
+      Kanji_PutText(font_, pos_x * PUYO_W, pos_y * PUYO_H,
                     scr_->off(), ss.str().c_str(), white);
     }
 
 #if 0
     // Debug message
     {
-      ostringstream ss;
-      ss << setw(10) << field.GetScore();
-      Kanji_PutText(font_, pos_x * 20, (pos_y + Field::MAP_HEIGHT + 1) * 20,
-                    scr_->off(), ss.str().c_str(), white);
-    }
-#endif
-
-    // Ojama
-    {
-      ostringstream ss;
-      ss << field.GetFixedOjama() << '(' << field.GetPendingOjama() << ')';
-      Kanji_PutText(font_, pos_x * 20, pos_y * 20,
-                    scr_->off(), ss.str().c_str(), white);
-    }
-
-    {
       int y = pos_y + Field::MAP_HEIGHT + 3 + field.player_id();
-      Kanji_PutText(font_, 20, y * 20, scr_->off(), debug_message.c_str(), white);
+      Kanji_PutText(font_, PUYO_W, y * PUYO_H, scr_->off(),
+                    debug_message.c_str(), white);
     }
-
 #endif
   }
 
   virtual void Flip() {
+    if (commentator_.get()) {
+      commentator_->tick();
+      commentator_->draw(scr_);
+    }
     scr_->syncOffscreen();
+    if (commentator_.get()) {
+      commentator_->drawMainChain(scr_);
+    }
     scr_->flip();
     SDL_FillRect(scr_->off(), NULL, SDL_MapRGB(scr_->off()->format, 0, 0, 0));
     scr_->clear();
@@ -242,8 +282,10 @@ private:
   }
 
   Screen* scr_;
+  Screen::Box bb_[2][6][14];
   Kanji_Font* font_;
   int key_state_[SDLK_LAST];
+  auto_ptr<Commentator> commentator_;
 };
 
 #endif
