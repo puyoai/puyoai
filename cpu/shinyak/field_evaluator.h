393575f5b9312c2d3abe8c7ca8b2ca96ca135542
MAYAH <mayah@mayah.jp>
2013-02-08 10:52:30 +0900

    removed unnecessary include

--- cpu/shinyak/field_evaluator.h
+++ cpu/shinyak/field_evaluator.h
@@ -1,12 +1,8 @@
 #ifndef __FIELD_EVALUATOR_H_
 #define __FIELD_EVALUATOR_H_
 
-#include <stdio.h>
-#include <vector>
-#include "field.h"
-#include "player_info.h"
+class Field;
 
-// TODO(mayah): non evaluation function should be move to Field.
 class FieldEvaluator {
 public:
     static double calculateEmptyFieldAvailability(const Field&);
