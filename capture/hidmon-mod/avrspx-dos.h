ab4b01ff122f96c7422741e1e139cb22c84b3f15
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2013-02-09 15:06:55 +0900

    Remove hidmon-mod for now per README

--- capture/hidmon-mod/avrspx-dos.h
+++
@@ -1,3 +0,0 @@
-#define C_WR_PAGE	0x4C
-#define C_LD_ADRX	0x4D
-#define	report_update(l)
--- capture/hidmon-mod/mymain.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-#include"hidasp.h"
-main(){
-  if (UsbInit(verbose_mode, 0, usb_serial) == 0) {
-    exit(1);
-  }
-  hidInit();
-}
