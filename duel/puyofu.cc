6f7177f7997ec942e4d73f9e34b528266608b32a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2013-02-09 21:38:52 +0900

    Add puyofu.cc

---
+++ duel/puyofu.cc
@@ -0,0 +1,48 @@
+#include "puyofu.h"
+
+#include <glog/logging.h>
+
+#include <core/field.h>
+#include <core/state.h>
+#include <util/field_util.h>
+
+void PuyoFu::setField(int pi, const Field& f, int state, int time) {
+  state &= ~STATE_YOU_CAN_PLAY;
+  if (!state)
+    return;
+
+  if ((state & STATE_YOU_GROUNDED) == 0)
+    return;
+
+  Move* move = new Move();
+  move->pi = pi;
+  move->f.reset(new Field(f));
+  const string& n = f.GetColorSequence();
+  CHECK(!n.empty());
+  move->next[6] = 0;
+  for (int i = 0; i < 6; i++) {
+    move->next[i] = f.GetNextPuyo(i) + '0';
+  }
+  move->time = time;
+  moves_.push_back(move);
+}
+
+void PuyoFu::emitFieldTransitionLog(FILE* fp, int pi) const {
+  Field f;
+  for (size_t i = 0; i < moves_.size(); i++) {
+    Move* m = moves_[i];
+    if (m->pi != pi)
+      continue;
+
+    string before, after;
+    GetRensimQueryString(f, &before);
+    if (before.empty())
+      before.push_back('0');
+    GetRensimQueryString(*m->f, &after);
+    if (after.empty())
+      after.push_back('0');
+    fprintf(fp, "%s %s %s\n", before.c_str(), m->next, after.c_str());
+    f = *m->f;
+  }
+}
+
