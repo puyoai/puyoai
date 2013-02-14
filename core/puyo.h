57846f3edfa647e1ea7a1ba4fe5e988ac2dbe372
MAYAH <mayah@mayah.jp>
2013-02-14 22:51:14 +0900

    Refactoring: Extracts Colors from field.h so that anyone can reuse it.

---
+++ core/puyo.h
@@ -0,0 +1,16 @@
+#ifndef CORE_PUYO_H_
+#define CORE_PUYO_H_
+
+enum Colors {
+  EMPTY = 0,
+  OJAMA = 1,
+  WALL = 2,
+  RED = 4,
+  BLUE = 5,
+  YELLOW = 6,
+  GREEN = 7,
+};
+
+typedef Colors PuyoColor;
+
+#endif
