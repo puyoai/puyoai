ed1f5f7dc00667bb164b7807a634535b988932ad
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2013-02-14 17:26:26 +0900

    Fix header guards

--- util/util.h
+++ util/util.h
@@ -1,6 +1,6 @@
-#ifndef CAPTURE_UTIL_H_
-#define CAPTURE_UTIL_H_
+#ifndef UTIL_UTIL_H_
+#define UTIL_UTIL_H_
 
 #define CLEAR_ARRAY(a) memset(a, 0, sizeof(a))
 
-#endif  // CAPTURE_UTIL_H_
+#endif  // UTIL_UTIL_H_
