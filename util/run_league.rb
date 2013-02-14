c2195f0a1d6acf4535186ec3aedb3849ce92fd54
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2013-02-14 09:55:03 +0900

    Tweak run_league.sh

--- util/run_league.rb
+++ util/run_league.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+#
+# A naive AI league runner
 
 NUM_WIN = 20
 
@@ -17,13 +19,19 @@ ENV['PUYO_NUM_WIN'] = NUM_WIN.to_s
 
 system("mkdir -p results")
 
+def normalize_ai_name(n)
+  n.sub('cpu/', '').tr('/', '_')
+end
+
 CPUS.size.times{|i|
   (i+1).upto(CPUS.size-1){|j|
     cpu1 = CPUS[i]
     cpu2 = CPUS[j]
+    log = ('results/' + normalize_ai_name(cpu1) +
+           '-vs-' + normalize_ai_name(cpu2))
+    puts log
     result = `duel/duel #{cpu1} #{cpu2}`
 
-    log = 'results/' + cpu1.tr('/', '_') + '_vs_' + cpu2.tr('/', '_')
     File.open(log, "w") do |of|
       of.puts result.split("\n").grep(/.* \/ .*/).join("\n")
     end
