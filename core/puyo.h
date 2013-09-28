7cd64719ebae81bed606c360b31524aef4381b1f
MAYAH <mayah@mayah.jp>
2013-09-28 23:04:11 +0900

    Moves some methods in cpu/shinyak/puyo.h to core/puyo.h

--- core/puyo.h
+++ core/puyo.h
@@ -1,6 +1,8 @@
 #ifndef CORE_PUYO_H_
 #define CORE_PUYO_H_
 
+#include <glog/logging.h>
+
 enum PuyoColor {
   EMPTY = 0,
   OJAMA = 1,
@@ -15,5 +17,26 @@ typedef unsigned char Puyo;
 
 // For backward compatibility
 typedef PuyoColor Colors;
+
+
+const int NUM_NORMAL_PUYO_COLORS = 4;
+
+inline PuyoColor normalPuyoColorOf(int index)
+{
+    DCHECK(0 <= index && index < NUM_NORMAL_PUYO_COLORS);
+    return static_cast<PuyoColor>(index + 4);
+}
+
+// Returns puyo color from character.
+PuyoColor puyoColorOf(char c);
+
+// Converts PuyoColor to character.
+char charOfPuyoColor(PuyoColor c);
+
+// Returns true if puyo is normal color puyo.
+inline bool isColorPuyo(PuyoColor color)
+{
+    return RED <= color && color <= GREEN;
+}
 
 #endif
