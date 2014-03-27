0fbfd5e214fa4a9f6a0c4d1c01467028241ea49d
Hitoshi Yoshida <git@peria.jp>
2014-03-28 01:00:02 +0900

    Add a test.

--- cpu/peria/player_test.cc
+++ cpu/peria/player_test.cc
@@ -1,14 +1,31 @@
 #include "player.h"
 
 #include <gtest/gtest.h>
+#include <vector>
 
 #include "base.h"
+
+class TestablePlayer : public Player {
+ public:
+  using Player::GetControls;
+};
 
 class PlayerTest : public testing::Test {
 };
 
-TEST_F(PlayerTest, Get) {
-  Player player;
-  player.set_field(Field("444400"));
-  EXPECT_EQ(kRed, player.field().Get(1, 1));
+TEST_F(PlayerTest, GetControls) {
+  struct TestData {
+    char* field;       // input
+    size_t ex_number;  // expected number of controls
+  } datas[] = {
+    {"", 22},
+  };
+
+  for (int i = 0; i < ARRAYSIZE(datas); ++i) {
+    vector<Player::Control> controls;
+    TestablePlayer player;
+    player.set_field(Field(string(datas[i].field)));
+    player.GetControls(&controls);
+    EXPECT_EQ(datas[i].ex_number, controls.size());
+  }
 }
