1f0056cd12a26e4e278e32b6b57fe5b3cede67a5
MAYAH <mayah@mayah.jp>
2014-06-12 08:55:59 +0900

    Refactoring: Move createConnector to Connector::create

--- src/core/server/connector/connector_manager_linux.cc
+++ src/core/server/connector/connector_manager_linux.cc
@@ -5,7 +5,6 @@
 #include <poll.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
 
@@ -24,94 +23,6 @@ DEFINE_bool(realtime, true, "use realtime");
 
 const int TIMEOUT_USEC = 1000000 / FPS;
 
-static unique_ptr<Connector> createConnector(int playerId, const string& programName)
-{
-    if (programName == "-")
-        return unique_ptr<Connector>(new HumanConnector);
-
-    if (programName.find("fifo:") == 0) {
-        string::size_type colon = programName.find(":", 5);
-        string uplink_fifo = programName.substr(5, colon - 5);
-        string downlink_fifo = programName.substr(colon + 1);
-        CHECK(mkfifo(uplink_fifo.c_str(), 0777) == 0);
-        CHECK(chmod(uplink_fifo.c_str(), 0777) == 0);
-        CHECK(mkfifo(downlink_fifo.c_str(), 0777) == 0);
-        CHECK(chmod(downlink_fifo.c_str(), 0777) == 0);
-        int uplink_fd = open(uplink_fifo.c_str(), O_RDONLY);
-        CHECK(uplink_fd >= 0);
-        int downlink_fd = open(downlink_fifo.c_str(), O_WRONLY);
-        CHECK(downlink_fd >= 0);
-
-        unique_ptr<Connector> connector(new PipeConnector(downlink_fd, uplink_fd));
-        connector->write("PingMessage");
-        (void)connector->read();
-
-        return connector;
-    }
-
-    // File descriptors.
-    int fd_field_status[2];
-    int fd_command[2];
-    int fd_cpu_error[2];
-
-    if (pipe(fd_field_status)) {
-        LOG(FATAL) << "Pipe error. " << strerror(errno);
-    }
-    if (pipe(fd_command)) {
-        LOG(FATAL) << "Pipe error. " << strerror(errno);
-    }
-    if (pipe(fd_cpu_error)) {
-        LOG(FATAL) << "Pipe error. " << strerror(errno);
-    }
-
-    pid_t pid = fork();
-    if (pid < 0) {
-        LOG(FATAL) << "Failed to fork. " << strerror(errno);
-    }
-
-    if (pid > 0) {
-        // Server.
-        LOG(INFO) << "Created a child process (pid = " << pid << ")";
-
-        unique_ptr<Connector> connector(new PipeConnector(fd_field_status[1], fd_command[0]));
-        close(fd_field_status[0]);
-        close(fd_command[1]);
-        close(fd_cpu_error[1]);
-
-        connector->write("PingMessage");
-        (void)connector->read();
-
-        return connector;
-    }
-
-    // Client.
-    if (dup2(fd_field_status[0], STDIN_FILENO) == -1) {
-        LOG(FATAL) << "Failed to dup2. " << strerror(errno);
-    }
-    if (dup2(fd_command[1], STDOUT_FILENO) == -1) {
-        LOG(FATAL) << "Failed to dup2. " << strerror(errno);
-    }
-    if (dup2(fd_cpu_error[1], STDERR_FILENO) == -1) {
-        LOG(FATAL) << "Failed to dup2. " << strerror(errno);
-    }
-
-    close(fd_field_status[0]);
-    close(fd_field_status[1]);
-    close(fd_command[0]);
-    close(fd_command[1]);
-    close(fd_cpu_error[0]);
-    close(fd_cpu_error[1]);
-
-    char filename[] = "Player1";
-    filename[6] += playerId; // TODO(mayah): What's this !!
-
-    if (execlp(programName.c_str(), filename) < 0)
-        PLOG(FATAL) << "Failed to start a child process. ";
-
-    LOG(FATAL) << "should not be reached.";
-    return unique_ptr<Connector>();
-}
-
 static int GetUsecFromStart(const struct timeval& start)
 {
     struct timeval now;
@@ -129,7 +40,7 @@ ConnectorManagerLinux::ConnectorManagerLinux(vector<string> program_names) :
     waitTimeout_(true)
 {
     for (int i = 0; i < program_names.size(); i++) {
-        connectors_.push_back(createConnector(i, program_names[i]));
+        connectors_.push_back(Connector::create(i, program_names[i]));
     }
 }
 
