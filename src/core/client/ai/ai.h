bd1164883069844f86770a6cbead981223f55320
MAYAH <mayah@mayah.jp>
2014-06-15 09:38:22 +0900

    Introduce thinkFast for AI.
    After ojama is dropped, we don't have much time to consider the next hand.
    When thinkFast is called, AI should return next hand as soon as possible.
    You will have at least 30ms (1frame) to consider.

--- src/core/client/ai/ai.h
+++ src/core/client/ai/ai.h
@@ -31,9 +31,17 @@ protected:
     virtual void gameHasEnd(const FrameData&) {}
 
     // think will be called when ai should decide the next hand.
-    // Basically, this will called when NEXT2 has appeared.
+    // Basically, this will be called when NEXT2 has appeared.
     // You will have around 300 ms to decide your hand.
     virtual DropDecision think(int frameId, const PlainField&, const Kumipuyo& next1, const Kumipuyo& next2) = 0;
+
+    // thinkFast will be called when ai should decide the next hand immediately.
+    // Basically this will be called when ojamas are dropped.
+    // You will have around 30 ms to decide your hand.
+    virtual DropDecision thinkFast(int frameId, const PlainField& field, const Kumipuyo& next1, const Kumipuyo& next2)
+    {
+        return think(frameId, field, next1, next2);
+    }
 
     // When enemy's puyo is grounded, this callback will be called.
     // You can detect enemy has started his rensa in this callback.
