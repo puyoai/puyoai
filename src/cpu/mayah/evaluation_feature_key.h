731bc032ef418293528674e3b1d72d7f036d028e
MAYAH <mayah@mayah.jp>
2014-06-17 23:35:57 +0900

    Trying to make 'learn' work again. Under implementation.

--- src/cpu/mayah/evaluation_feature_key.h
+++ src/cpu/mayah/evaluation_feature_key.h
@@ -27,10 +27,42 @@ inline EvaluationFeatureKey toEvaluationFeatureKey(int ith)
     return static_cast<EvaluationFeatureKey>(ith);
 }
 
+inline std::string toString(EvaluationFeatureKey key)
+{
+    switch (key) {
+#define DEFINE_PARAM(key) case key: return #key;
+#define DEFINE_SPARSE_PARAM(key, value) /* ignored */
+#include "evaluation_feature.tab"
+#undef DEFINE_PARAM
+#undef DEFINE_SPARSE_PARAM
+    case SIZE_OF_EVALUATION_FEATURE_KEY:
+        break;
+    }
+
+    CHECK(false) << "Unexpected EvaluationFeatureKey: " << static_cast<int>(key);
+    return "";
+}
+
 inline EvaluationSparseFeatureKey toEvaluationSparseFeatureKey(int ith)
 {
     DCHECK(0 <= ith && ith < SIZE_OF_EVALUATION_SPARSE_FEATURE_KEY);
     return static_cast<EvaluationSparseFeatureKey>(ith);
+}
+
+inline std::string toString(EvaluationSparseFeatureKey key)
+{
+    switch (key) {
+#define DEFINE_PARAM(key) /* ignored */
+#define DEFINE_SPARSE_PARAM(key, value) case key: return #key;
+#include "evaluation_feature.tab"
+#undef DEFINE_PARAM
+#undef DEFINE_SPARSE_PARAM
+    case SIZE_OF_EVALUATION_SPARSE_FEATURE_KEY:
+        break;
+    }
+
+    CHECK(false) << "Unexpected EvaluationSparseFeatureKey: " << static_cast<int>(key);
+    return "";
 }
 
 #endif
