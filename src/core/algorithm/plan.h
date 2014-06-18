800b4f520bfe77b2d3728c669ba03cf62ffe1e7b
MAYAH <mayah@mayah.jp>
2014-06-18 10:06:24 +0900

    Try to re-implement clearner in cpu/mayah

--- src/core/algorithm/plan.h
+++ src/core/algorithm/plan.h
@@ -64,6 +64,7 @@ public:
 
     const CoreField& field() const { return field_; }
     const std::vector<Decision>& decisions() const { return decisions_; }
+    const Decision& decision(int nth) const { return decisions_[nth]; }
     const RensaResult& rensaResult() const { return rensaResult_; }
 
     int chains() const { return rensaResult_.chains; }
