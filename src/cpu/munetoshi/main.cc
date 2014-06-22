0920e42740289c8f1ba1b51cef4b1c8479895a44
munetoshi <munetoshi@Mi-MacBook.local>
2014-06-22 22:06:41 +0900

    Add munetoshi ai

---
+++ src/cpu/munetoshi/main.cc
@@ -0,0 +1,14 @@
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+#include "ai.h"
+
+int main(int argc, char* argv[]) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+
+  munetoshi::AI().runLoop();
+
+  return 0;
+}
