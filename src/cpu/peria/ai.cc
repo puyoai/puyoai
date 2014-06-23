954bd4412fac07032720a7fe3d9a5c5d62e1662f
Hitoshi Yoshida <git@peria.jp>
2014-06-24 00:52:46 +0900

    Update cpu/peria

--- src/cpu/peria/ai.cc
+++ src/cpu/peria/ai.cc
@@ -1,5 +1,7 @@
 #include "cpu/peria/ai.h"
 
+#include "cpu/peria/game.h"
+
 namespace peria {
 
 Ai::Ai() : name_("peria") {}
@@ -7,6 +9,8 @@ Ai::Ai() : name_("peria") {}
 Ai::~Ai() {}
 
 void Ai::RunLoop() {
+  Game game;
+  while (connector_.Receive(&game)) {}
 }
 
 }  // namespace peria
---
+++ src/cpu/peria/game.cc
@@ -0,0 +1,2 @@
+#include "cpu/peria/game.h"
+
