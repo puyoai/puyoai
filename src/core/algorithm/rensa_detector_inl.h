a2abf14272f3699766564bb2f730f451ccec8f8b
munetoshi <munetoshi.iskw@gmail.com>
2014-06-29 16:23:14 +0900

    Fix a bug of RensaDetector::FLOAT, which returns always 0 for nesessaryPuyos

--- src/core/algorithm/rensa_detector_inl.h
+++ src/core/algorithm/rensa_detector_inl.h
@@ -62,11 +62,12 @@ static inline void tryFloatFire(
     return;
 
   int necessaryPuyos = 4 - field.countConnectedPuyos(x, y);
+  int restPuyos = necessaryPuyos;
   CoreField f(field);
 
   int dx = x - 1;
   // float puyo col dx
-  for (; dx <= x + 1 && necessaryPuyos > 0; ++dx) {
+  for (; dx <= x + 1 && restPuyos > 0; ++dx) {
     if (dx <= 0 || CoreField::WIDTH < dx) {
      continue;
     }
@@ -76,25 +77,25 @@ static inline void tryFloatFire(
     if (dx != x) {
       if (field.color(dx, y) != PuyoColor::EMPTY) {
         continue;
-      } else { // necessaryPuyos must be more than 0
+      } else { // restPuyos must be more than 0
         f.unsafeSet(dx, y, c);
-        --necessaryPuyos;
+        --restPuyos;
       }
     }
 
     int dy_min = y - 1;
     // Check under y
-    for (; necessaryPuyos > 0 && dy_min > 0 && field.color(dx ,dy_min) == PuyoColor::EMPTY;
+    for (; restPuyos > 0 && dy_min > 0 && field.color(dx ,dy_min) == PuyoColor::EMPTY;
         --dy_min) {
       f.unsafeSet(dx, dy_min, c);
-      --necessaryPuyos;
+      --restPuyos;
     }
 
     // Check over y
     for (int dy = y + 1;
-        necessaryPuyos > 0 && dy <= 13 && field.color(dx ,dy) == PuyoColor::EMPTY; ++dy) {
+        restPuyos > 0 && dy <= 13 && field.color(dx ,dy) == PuyoColor::EMPTY; ++dy) {
       f.unsafeSet(dx, dy, c);
-      --necessaryPuyos;
+      --restPuyos;
     }
 
     // Fill ojama
@@ -105,7 +106,7 @@ static inline void tryFloatFire(
     f.recalcHeightOn(dx);
   }
 
-  if (necessaryPuyos <= 0) {
+  if (restPuyos <= 0) {
     callback(&f, dx, c, necessaryPuyos);
   }
 }
