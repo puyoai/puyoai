bcb14dd6b01020dee9459a82c01402beb4749d13
Hitoshi Yoshida <git@peria.jp>
2014-07-03 21:48:19 +0900

    Clean up peria AI

--- src/cpu/peria/ai.cc
+++ src/cpu/peria/ai.cc
@@ -1,20 +1,11 @@
 #include "cpu/peria/ai.h"
 
-#include "core/constant.h"
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+#include "core/algorithm/plan.h"
 #include "core/field/core_field.h"
 #include "core/field/rensa_result.h"
-
-namespace {
-
-bool IsQuick(RensaResult result) {
-  int frame = result.frames;
-  frame -= FRAMES_AFTER_NO_DROP;
-  frame -= FRAMES_AFTER_VANISH * result.chains;
-  frame -= FRAMES_AFTER_DROP * (result.chains - 1);
-  return frame % FRAMES_DROP_1_LINE == 0;
-}
-
-}  // namespace
 
 namespace peria {
 
@@ -22,33 +13,39 @@ Ai::Ai(): ::AI("peria") {}
 
 Ai::~Ai() {}
 
-DropDecision Ai::think(int /*frame_id*/,
-		       const PlainField& /*field*/,
-		       const KumipuyoSeq& /*seq*/) {
-  return DropDecision();
+DropDecision Ai::think(int frame_id,
+		       const PlainField& field,
+		       const KumipuyoSeq& seq) {
+  UNUSED_VARIABLE(frame_id);
+
+  LOG(INFO) << CoreField(field).toDebugString() << seq.toString();
+  
+  Decision best;
+  int score = -1;
+  Plan::iterateAvailablePlans(
+      CoreField(field),
+      seq,
+      2,
+      [&best, &score](const RefPlan& plan) {
+        int s = 0;
+        if (plan.isRensaPlan()) {
+          s += plan.rensaResult().chains * 10;
+          s -= plan.decisions().size();
+        }
+
+        if (score < s) {
+          score = s;
+          best = plan.decisions().front();
+        }
+      });
+
+  return DropDecision(best);
 }
 
 void Ai::gameWillBegin(const FrameData& /*frame_data*/) {
-  zenkeshi_[0] = false;
-  zenkeshi_[1] = false;
 }
 
-void Ai::enemyGrounded(const FrameData& frame_data) {
-  const PlayerFrameData& enemy = frame_data.enemyPlayerFrameData();
-
-  CoreField field(enemy.field);
-  RensaResult result = field.simulate();
-  if (result.score > 0) {
-    bool is_quick = IsQuick(result);
-    // attack_.end_at = frame_data.frame_id + result.frames;
-    // attack_.tsubushi = Is潰し();
-    // attack_.saisoku = Is催促(); // 連鎖数と続き部分からの連鎖数で判断
-    // attack_.honsen = Is本線();  // 消えたぷよ数で判断
-  } else {
-    // attack_.Reset();
-    // 3 手完全探索する
-    // 発火可能な最大点数とその発火までの frame 数を記録
-  }
+void Ai::enemyGrounded(const FrameData& /*frame_data*/) {
 }
 
 }  // namespace peria
