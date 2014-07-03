b36b848f95bdcf199d03d19ceaf70c4263b5ca62
Hitoshi Yoshida <git@peria.jp>
2014-07-03 22:47:05 +0900

    Search 3rd hand in peria AI.

--- src/cpu/peria/ai.cc
+++ src/cpu/peria/ai.cc
@@ -2,10 +2,12 @@
 
 #include <gflags/gflags.h>
 #include <glog/logging.h>
+#include <sstream>
 
 #include "core/algorithm/plan.h"
 #include "core/field/core_field.h"
 #include "core/field/rensa_result.h"
+#include "core/puyo_color.h"
 
 namespace peria {
 
@@ -19,26 +21,55 @@ DropDecision Ai::think(int frame_id,
   UNUSED_VARIABLE(frame_id);
 
   LOG(INFO) << CoreField(field).toDebugString() << seq.toString();
-  
+  std::ostringstream message;
+
   Decision best;
   int score = -1;
-  Plan::iterateAvailablePlans(
-      CoreField(field),
-      seq,
-      2,
+  Plan::iterateAvailablePlans(CoreField(field), seq, 2,
       [&best, &score](const RefPlan& plan) {
+        if (score > 70 * 5 * 12)
+          return;
+
         int s = 0;
         if (plan.isRensaPlan()) {
           s = plan.rensaResult().score;
         }
-
+            
         if (score < s) {
           score = s;
           best = plan.decisions().front();
         }
       });
 
-  return DropDecision(best);
+
+  int depth = seq.size();
+  KumipuyoSeq seq2(seq);
+  seq2.resize(depth + 1);
+  for (uint8_t i = RED; i <= GREEN; ++i) {
+    seq2.setAxis(depth, static_cast<PuyoColor>(i));
+    for (uint8_t j = RED; j <= GREEN; ++j) {
+      seq2.setChild(depth, static_cast<PuyoColor>(j));
+  Plan::iterateAvailablePlans(CoreField(field), seq2, depth + 1,
+      [&best, &score](const RefPlan& plan) {
+        if (score > 70 * 5 * 12)
+          return;
+
+        int s = 0;
+        if (plan.isRensaPlan()) {
+          s = plan.rensaResult().score;
+        }
+            
+        if (score < s) {
+          score = s;
+          best = plan.decisions().front();
+        }
+      });
+    }
+  }
+  message << "X-R:" << best.x << "-" << best.r
+          << "_SCORE:" << score << "   ";
+
+  return DropDecision(best, message.str());
 }
 
 void Ai::gameWillBegin(const FrameData& /*frame_data*/) {
