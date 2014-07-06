60a26bbf379a0ce114f62270ffe6b9c301d7eac2
MAYAH <mayah@mayah.jp>
2014-07-06 11:50:56 +0900

    Add generateSequenceWithMt19937 and generateSequenceWithSeed.

--- src/core/sequence_generator.h
+++ src/core/sequence_generator.h
@@ -1,8 +1,12 @@
 #ifndef CORE_SEQUENCE_GENERATOR_H_
 #define CORE_SEQUENCE_GENERATOR_H_
 
+#include <random>
+
 #include "core/kumipuyo.h"
 
 KumipuyoSeq generateSequence();
+KumipuyoSeq generateSequenceWithSeed(int seed);
+KumipuyoSeq generateSequenceWithMt19937(std::mt19937);
 
 #endif
