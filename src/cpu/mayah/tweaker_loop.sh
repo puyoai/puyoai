46dfcc3f306144bdedb9bd54af0bbc2e1475980a
MAYAH <mayah@mayah.jp>
2014-07-07 19:49:46 +0900

    Refactoring: tweaker_loop.sh

--- src/cpu/mayah/tweaker_loop.sh
+++ src/cpu/mayah/tweaker_loop.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -eux
+
 function sum_max_score() {
     local filename=$1
     cat "${filename}" | grep -e 'max score' | cut -d ' ' -f 4 | xargs echo | sed -e 's/ / + /g' | xargs expr
@@ -7,14 +9,19 @@ function sum_max_score() {
 
 function run_tweak_and_duel() {
     rm ./cpu/mayah/run2.err
-    cp ./cpu/mayah/best_feature.txt ./cpu/mayah/feature.txt
     ./cpu/mayah/tweaker --feature=./cpu/mayah/feature.txt
     for i in {0..99}; do
         echo "seed $i"
         ./duel/duel --num_duel=1 --realtime=false --no_timeout=true --seed="$i" --use_gui=false --use_cui=false --httpd=false -- cpu/mayah/run_for_tweaker_1p.sh cpu/mayah/run_for_tweaker_2p.sh
     done
 
-    score=`sum_max_score ./cpu/mayah/run2.err`
+    sum_max_score ./cpu/mayah/run2.err
+}
+
+function run_one_time_loop() {
+    cp ./cpu/mayah/best_feature.txt ./cpu/mayah/feature.txt
+    score=`run_tweak_and_duel`
+
     if [ -e ./cpu/mayah/best_score.txt ]; then
         bestscore=`cat ./cpu/mayah/best_score.txt`
     else
@@ -30,10 +37,6 @@ function run_tweak_and_duel() {
     else
         cp ./cpu/mayah/best_feature.txt ./cpu/mayah/feature.txt
     fi
-
-    echo "done"
 }
 
-run_tweak_and_duel
-
-
+run_one_time_loop
