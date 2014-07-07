ac1b5c1cbfa69816e6d84a4eed9327562f3f8265
MAYAH <mayah@mayah.jp>
2014-07-07 23:19:05 +0900

    Fix tweaker loop

--- src/cpu/mayah/tweaker_loop.sh
+++ src/cpu/mayah/tweaker_loop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 function sum_max_score() {
     local filename=$1
@@ -10,23 +10,22 @@ function sum_max_score() {
 function run_tweak_and_duel() {
     rm ./cpu/mayah/run2.err
     ./cpu/mayah/tweaker --feature=./cpu/mayah/feature.txt
-    for i in {0..99}; do
-        echo "seed $i"
-        ./duel/duel --num_duel=1 --realtime=false --no_timeout=true --seed="$i" --use_gui=false --use_cui=false --httpd=false -- cpu/mayah/run_for_tweaker_1p.sh cpu/mayah/run_for_tweaker_2p.sh
+    for i in {0..29}; do
+        ./duel/duel --num_duel=1 --realtime=false --no_timeout=true --seed="$i" --use_gui=false --use_cui=false --httpd=false -- cpu/mayah/run_for_tweaker_1p.sh cpu/mayah/run_for_tweaker_2p.sh > /dev/null 2> /dev/null
     done
 
     sum_max_score ./cpu/mayah/run2.err
 }
 
 function run_one_time_loop() {
-    cp ./cpu/mayah/best_feature.txt ./cpu/mayah/feature.txt
-    score=`run_tweak_and_duel`
-
-    if [ -e ./cpu/mayah/best_score.txt ]; then
+    if [ -e ./cpu/mayah/best_score.txt -a -e ./cpu/mayah/best_feature.txt ]; then
         bestscore=`cat ./cpu/mayah/best_score.txt`
+        cp ./cpu/mayah/best_feature.txt ./cpu/mayah/feature.txt
     else
         bestscore=0
     fi
+
+    score=`run_tweak_and_duel`
 
     echo "score = $score"
     echo "current bestscore = $bestscore"
