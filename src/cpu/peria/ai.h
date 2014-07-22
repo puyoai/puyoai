0fa3f8380cade26e3b7f083ac1026f72d7031690
Hitoshi Yoshida <git@peria.jp>
2014-07-22 23:32:34 +0900

    Add a routine to check opponent's attack.

--- src/cpu/peria/ai.h
+++ src/cpu/peria/ai.h
@@ -3,7 +3,10 @@
 
 #include "core/client/ai/ai.h"
 
+#include <memory>  // available with C++11
+
 namespace peria {
+
 
 class Ai : public ::AI {
  public:
@@ -11,11 +14,16 @@ class Ai : public ::AI {
   virtual ~Ai();
 
  protected:
+  struct Attack;
+
   virtual DropDecision think(int frame_id,
                              const PlainField& field,
                              const KumipuyoSeq& seq) override;
   virtual void gameWillBegin(const FrameData& frame_data) override;
   virtual void enemyGrounded(const FrameData& frame_data) override;
+
+  // Information about opponent's attacks.
+  std::unique_ptr<Attack> attack_;
 };
 
 }  // namespace peria
