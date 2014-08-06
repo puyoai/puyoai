9360d9749dcd5edd100b8831768e1b9fbeeb5f2d
MAYAH <mayah@mayah.jp>
2014-08-06 22:54:34 +0900

    Remove PuyoSet::toInt(). Add test for relatively large numbers.

--- src/core/algorithm/puyo_set_test.cc
+++ src/core/algorithm/puyo_set_test.cc
@@ -2,15 +2,33 @@
 
 #include <gtest/gtest.h>
 
+TEST(PuyoSetTest, ctor)
+{
+    PuyoSet set1;
+    EXPECT_EQ(0, set1.red());
+    EXPECT_EQ(0, set1.blue());
+    EXPECT_EQ(0, set1.yellow());
+    EXPECT_EQ(0, set1.green());
+
+    PuyoSet set2(1, 2, 3, 4);
+    EXPECT_EQ(1, set2.red());
+    EXPECT_EQ(2, set2.blue());
+    EXPECT_EQ(3, set2.yellow());
+    EXPECT_EQ(4, set2.green());
+}
+
 TEST(PuyoSetTest, addPuyoColor)
 {
     PuyoSet set;
     set.add(PuyoColor::RED, 1);
+    set.add(PuyoColor::BLUE, 2);
+    set.add(PuyoColor::YELLOW, 3);
+    set.add(PuyoColor::GREEN, 4);
 
     EXPECT_EQ(1, set.red());
-    EXPECT_EQ(0, set.blue());
-    EXPECT_EQ(0, set.yellow());
-    EXPECT_EQ(0, set.green());
+    EXPECT_EQ(2, set.blue());
+    EXPECT_EQ(3, set.yellow());
+    EXPECT_EQ(4, set.green());
 }
 
 TEST(PuyoSetTest, addPuyoSet)
@@ -29,3 +47,32 @@ TEST(PuyoSetTest, addPuyoSet)
     EXPECT_EQ(0, set.yellow());
     EXPECT_EQ(1, set.green());
 }
+
+TEST(PuyoSetTest, addPuyoSetLarge)
+{
+    PuyoSet set1(123456, 123456, 123456, 123456);
+    PuyoSet set2(1, 2, 3, 4);
+
+    PuyoSet set;
+    set.add(set1);
+    set.add(set2);
+
+    EXPECT_EQ(123457, set.red());
+    EXPECT_EQ(123458, set.blue());
+    EXPECT_EQ(123459, set.yellow());
+    EXPECT_EQ(123460, set.green());
+}
+
+TEST(PuyoSetTest, subPuyoSet)
+{
+    PuyoSet set1(1, 2, 3, 4);
+    PuyoSet set2(2, 2, 2, 2);
+
+    set1.sub(set2);
+
+    EXPECT_EQ(0, set1.red());
+    EXPECT_EQ(0, set1.blue());
+    EXPECT_EQ(1, set1.yellow());
+    EXPECT_EQ(2, set1.green());
+}
+
