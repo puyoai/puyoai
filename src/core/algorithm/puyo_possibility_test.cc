1500d94b2335b1cca055b4eca9e668971796d9e9
MAYAH <mayah@mayah.jp>
2014-08-07 22:01:48 +0900

    Add more PuyoPossibility test.

--- src/core/algorithm/puyo_possibility_test.cc
+++ src/core/algorithm/puyo_possibility_test.cc
@@ -2,13 +2,45 @@
 
 #include <gtest/gtest.h>
 
-TEST(TsumoPossibilityTest, Possibility)
+TEST(TsumoPossibilityTest, possibility)
 {
     TsumoPossibility::initialize();
 
-    EXPECT_EQ(TsumoPossibility::possibility(0, 0, 0, 0, 0), 1.0);
-    EXPECT_EQ(TsumoPossibility::possibility(1, 0, 0, 0, 0), 1.0);
-    EXPECT_EQ(TsumoPossibility::possibility(1, 1, 0, 0, 0), 1.0 / 4.0);
-    EXPECT_EQ(TsumoPossibility::possibility(2, 1, 1, 0, 0), 1.0 / 8.0);
-    EXPECT_EQ(TsumoPossibility::possibility(2, 2, 0, 0, 0), 1.0 / 16.0);
+    EXPECT_EQ(1.0, TsumoPossibility::possibility(0, 0, 0, 0, 0));
+
+    EXPECT_EQ(4.0 / 4.0, TsumoPossibility::possibility(1, 0, 0, 0, 0));
+    EXPECT_EQ(1.0 / 4.0, TsumoPossibility::possibility(1, 1, 0, 0, 0));
+    EXPECT_EQ(1.0 / 4.0, TsumoPossibility::possibility(1, 0, 1, 0, 0));
+    EXPECT_EQ(1.0 / 4.0, TsumoPossibility::possibility(1, 0, 0, 1, 0));
+    EXPECT_EQ(1.0 / 4.0, TsumoPossibility::possibility(1, 0, 0, 0, 1));
+    EXPECT_EQ(0.0 / 4.0, TsumoPossibility::possibility(1, 1, 1, 0, 0));
+    EXPECT_EQ(0.0 / 4.0, TsumoPossibility::possibility(1, 0, 1, 1, 0));
+    EXPECT_EQ(0.0 / 4.0, TsumoPossibility::possibility(1, 0, 0, 1, 1));
+    EXPECT_EQ(0.0 / 4.0, TsumoPossibility::possibility(1, 1, 0, 0, 1));
+
+    EXPECT_EQ(16.0 / 16.0, TsumoPossibility::possibility(2, 0, 0, 0, 0));
+
+    EXPECT_EQ(7.0 / 16.0, TsumoPossibility::possibility(2, 1, 0, 0, 0));
+    EXPECT_EQ(7.0 / 16.0, TsumoPossibility::possibility(2, 0, 1, 0, 0));
+    EXPECT_EQ(7.0 / 16.0, TsumoPossibility::possibility(2, 0, 0, 1, 0));
+    EXPECT_EQ(7.0 / 16.0, TsumoPossibility::possibility(2, 0, 0, 0, 1));
+
+    EXPECT_EQ(1.0 / 16.0, TsumoPossibility::possibility(2, 2, 0, 0, 0));
+    EXPECT_EQ(1.0 / 16.0, TsumoPossibility::possibility(2, 0, 2, 0, 0));
+    EXPECT_EQ(1.0 / 16.0, TsumoPossibility::possibility(2, 0, 0, 2, 0));
+    EXPECT_EQ(1.0 / 16.0, TsumoPossibility::possibility(2, 0, 0, 0, 2));
+
+    EXPECT_EQ(2.0 / 16.0, TsumoPossibility::possibility(2, 1, 1, 0, 0));
+    EXPECT_EQ(2.0 / 16.0, TsumoPossibility::possibility(2, 1, 0, 1, 0));
+    EXPECT_EQ(2.0 / 16.0, TsumoPossibility::possibility(2, 1, 0, 0, 1));
+    EXPECT_EQ(2.0 / 16.0, TsumoPossibility::possibility(2, 0, 1, 1, 0));
+    EXPECT_EQ(2.0 / 16.0, TsumoPossibility::possibility(2, 0, 1, 0, 1));
+    EXPECT_EQ(2.0 / 16.0, TsumoPossibility::possibility(2, 0, 0, 1, 1));
+
+    // (5!/2!) / 4^5
+    EXPECT_EQ(15.0 / 256.0, TsumoPossibility::possibility(5, 2, 1, 1, 1));
+
+    // RGBY-R or RGBY-G or RGBY-B or RGBY-Y
+    // 4 * (5!/2!) / 4^5
+    EXPECT_EQ(15.0 / 64.0, TsumoPossibility::possibility(5, 1, 1, 1, 1));
 }
