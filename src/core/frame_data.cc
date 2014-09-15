954a1d0fb9983b811ea61680992dc2882aa23dbc
MAYAH <mayah@mayah.jp>
2014-09-15 23:44:34 +0900

    Add cpu/mayah/run_v.sh for VLOG(1).

--- src/core/frame_data.cc
+++ src/core/frame_data.cc
@@ -1,5 +1,11 @@
 #include "core/frame_data.h"
 
+#include "core/field/field_pretty_printer.h"
+
+#include <sstream>
+
+using namespace std;
+
 PlayerFrameData::PlayerFrameData(const PlainField& field, const KumipuyoSeq& seq, const KumipuyoPos& pos,
                                  const UserState& userState, int score, int ojama) :
     field(field),
@@ -9,4 +15,15 @@ PlayerFrameData::PlayerFrameData(const PlainField& field, const KumipuyoSeq& seq
     score(score),
     ojama(ojama)
 {
+}
+
+string FrameData::toString() const
+{
+    stringstream ss;
+    ss << "ID=" << id << endl;
+    ss << FieldPrettyPrinter::toStringFromMultipleFields(
+        playerFrameData[0].field, playerFrameData[0].kumipuyoSeq,
+        playerFrameData[1].field, playerFrameData[1].kumipuyoSeq) << endl;
+
+    return ss.str();
 }
