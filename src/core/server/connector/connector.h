a01a17ab550861d2b9a2e0b54435416c8f9faa99
MAYAH <mayah@mayah.jp>
2014-09-19 10:24:57 +0900

    Extract PipeConnector from connector.

--- src/core/server/connector/connector.h
+++ src/core/server/connector/connector.h
@@ -1,8 +1,6 @@
 #ifndef CORE_SERVER_CONNECTOR_CONNECTOR_H_
 #define CORE_SERVER_CONNECTOR_CONNECTOR_H_
 
-#include <cstdio>
-#include <memory>
 #include <string>
 
 #include "base/base.h"
@@ -32,28 +30,6 @@ public:
 
 protected:
     int playerId_;
-};
-
-class PipeConnector : public Connector {
-public:
-    PipeConnector(int playerId, int writerFd, int readerFd);
-    virtual ~PipeConnector();
-
-    virtual void write(const ConnectorFrameRequest&) override;
-    virtual void writeString(const std::string&) override;
-    virtual ConnectorFrameResponse read() override;
-    virtual bool isHuman() const override { return false; }
-    virtual bool alive() const override { return alive_; }
-    virtual void setAlive(bool flag) override { alive_ = flag; }
-    virtual bool pollable() const override { return true; }
-    virtual int readerFd() const override { return readerFd_; }
-
-private:
-    bool alive_ = true;
-    int writerFd_;
-    int readerFd_;
-    FILE* writer_;
-    FILE* reader_;
 };
 
 #endif
---
+++ src/core/server/connector/pipe_connector.h
@@ -0,0 +1,32 @@
+#ifndef CORE_SERVER_CONNECTOR_PIPE_CONNECTOR_H_
+#define CORE_SERVER_CONNECTOR_PIPE_CONNECTOR_H_
+
+#include <cstdio>
+#include <memory>
+
+#include "core/server/connector/connector.h"
+
+class PipeConnector : public Connector {
+public:
+    PipeConnector(int playerId, int writerFd, int readerFd);
+    virtual ~PipeConnector();
+
+    virtual void write(const ConnectorFrameRequest&) override;
+    virtual void writeString(const std::string&) override;
+    virtual ConnectorFrameResponse read() override;
+    virtual bool isHuman() const override { return false; }
+    virtual bool alive() const override { return alive_; }
+    virtual void setAlive(bool flag) override { alive_ = flag; }
+    virtual bool pollable() const override { return true; }
+    virtual int readerFd() const override { return readerFd_; }
+
+private:
+    bool alive_ = true;
+    int writerFd_;
+    int readerFd_;
+    FILE* writer_;
+    FILE* reader_;
+};
+
+#endif
+
