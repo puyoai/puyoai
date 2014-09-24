6810b3f53d3cf3c8d6bc18ea20bde63dece96b73
MAYAH <mayah@mayah.jp>
2014-09-25 08:45:17 +0900

    Extract constants from PlainField to FieldConstant.

--- src/core/plain_field.h
+++ src/core/plain_field.h
@@ -2,15 +2,12 @@
 #define CORE_PLAIN_FIELD_H_
 
 #include <string>
+
+#include "core/field_constant.h"
 #include "core/puyo_color.h"
 
-class PlainField {
+class PlainField : public FieldConstant{
 public:
-    static const int WIDTH = 6;
-    static const int HEIGHT = 12;
-    static const int MAP_WIDTH = 1 + WIDTH + 1;
-    static const int MAP_HEIGHT = 1 + HEIGHT + 3;
-
     PlainField();
     PlainField(const std::string& url);
 
