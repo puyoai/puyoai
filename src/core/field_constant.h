486d577230e840e6a1b8ec4e11e1426102b42daf
MAYAH <mayah@mayah.jp>
2014-09-25 08:45:17 +0900

    Extract constants from PlainField to FieldConstant.

---
+++ src/core/field_constant.h
@@ -0,0 +1,12 @@
+#ifndef CORE_FIELD_CONSTANT_H_
+#define CORE_FIELD_CONSTANT_H_
+
+class FieldConstant {
+public:
+    static const int WIDTH = 6;
+    static const int HEIGHT = 12;
+    static const int MAP_WIDTH = 1 + WIDTH + 1;
+    static const int MAP_HEIGHT = 1 + HEIGHT + 3;
+};
+
+#endif
