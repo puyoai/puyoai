c8d9984549c986401e7793c2671aa231eed625a1
MAYAH <mayah@mayah.jp>
2014-09-28 21:52:30 +0900

    Add BookField::MatchResult for representing match result of book field.

--- src/cpu/mayah/book_field.h
+++ src/cpu/mayah/book_field.h
@@ -2,16 +2,26 @@
 #define CPU_MAYAH_BOOK_FIELD_H_
 
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include "core/plain_field.h"
 
 class BookField {
 public:
+    struct MatchResult {
+        MatchResult(double score, int count) : score(score), count(count) {}
+
+        friend bool operator==(const MatchResult& lhs, const MatchResult& rhs) { return std::tie(lhs.score, lhs.count) == std::tie(rhs.score, rhs.count); }
+
+        double score = 0.0;
+        int count = 0;
+    };
+
     BookField(const std::string& name, const std::vector<std::string>& field, double defaultScore = 1);
 
     // match returns the matched score. If not matched, 0 will be returned.
-    double match(const PlainField&) const;
+    MatchResult match(const PlainField&) const;
 
     void merge(const BookField&);
     BookField mirror() const;
