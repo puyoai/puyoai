d99669ad71d3047143b14757f8f7595dd6205b6f
MAYAH <mayah@mayah.jp>
2014-10-01 19:33:30 +0900

    Add iterateAvailablePlans test.

--- src/core/algorithm/plan_test.cc
+++ src/core/algorithm/plan_test.cc
@@ -1,12 +1,13 @@
 #include "core/algorithm/plan.h"
 
 #include <gtest/gtest.h>
+#include "core/constant.h"
 #include "core/field/core_field.h"
 #include "core/kumipuyo.h"
 
 using namespace std;
 
-TEST(Plan, IteratorAvailablePlans)
+TEST(Plan, iterateAvailablePlans)
 {
     CoreField field("  YY  ");
     KumipuyoSeq kumipuyoSeq = {
@@ -31,6 +32,31 @@ TEST(Plan, IteratorAvailablePlans)
     EXPECT_TRUE(found);
 }
 
+TEST(Plan, iterateAvailablePlansWithRensa)
+{
+    CoreField field("  RR  ");
+    KumipuyoSeq seq("RRBB");
+
+    bool found = false;
+    Plan plan;
+    Plan::iterateAvailablePlans(field, seq, 2, [&found, &plan](const RefPlan& p) {
+        if (p.decisions().size() == 1 && p.decisions().front() == Decision(3, 2)) {
+            found = true;
+            plan = p.toPlan();
+        }
+    });
+
+    EXPECT_TRUE(found);
+    EXPECT_EQ(1, plan.rensaResult().chains);
+    EXPECT_EQ(40, plan.rensaResult().score);
+    EXPECT_EQ(FRAMES_VANISH_ANIMATION, plan.rensaResult().frames);
+    EXPECT_TRUE(plan.rensaResult().quick);
+
+    EXPECT_EQ(FRAMES_TO_DROP_FAST[10] + FRAMES_GROUNDING + FRAMES_VANISH_ANIMATION,
+              plan.totalFrames());
+    EXPECT_EQ(0, plan.initiatingFrames());
+}
+
 TEST(Plan, numChigiri)
 {
     CoreField cf("  O   ");
@@ -46,4 +72,3 @@ TEST(Plan, numChigiri)
 
     EXPECT_TRUE(found);
 }
-
