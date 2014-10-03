33c69d32657ce247458d70f04a6f0b5b1fe2140c
MAYAH <mayah@mayah.jp>
2014-10-03 23:37:01 +0900

    stylefix.

--- src/core/kumipuyo.h
+++ src/core/kumipuyo.h
@@ -11,24 +11,11 @@ public:
     constexpr Kumipuyo() : axis(PuyoColor::EMPTY), child(PuyoColor::EMPTY) {}
     constexpr Kumipuyo(PuyoColor axis, PuyoColor child) : axis(axis), child(child) {}
 
-    std::string toString() const
-    {
-        char tmp[] = "  ";
-        tmp[0] = toChar(axis);
-        tmp[1] = toChar(child);
+    std::string toString() const;
 
-        return tmp;
-    }
+    bool isValid() const { return isNormalColor(axis) && isNormalColor(child); }
 
-    bool isValid() const
-    {
-        return isNormalColor(axis) && isNormalColor(child);
-    }
-
-    friend bool operator==(const Kumipuyo& lhs, const Kumipuyo& rhs)
-    {
-        return lhs.axis == rhs.axis && lhs.child == rhs.child;
-    }
+    friend bool operator==(const Kumipuyo& lhs, const Kumipuyo& rhs) { return lhs.axis == rhs.axis && lhs.child == rhs.child; }
     friend bool operator!=(const Kumipuyo& lhs, const Kumipuyo& rhs) { return !(lhs == rhs); }
 
 public:
