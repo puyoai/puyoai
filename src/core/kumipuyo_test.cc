2e6122dbf516a46ceff419d51b6794ffcff5c11d
MAYAH <mayah@mayah.jp>
2014-10-03 23:46:18 +0900

    Add Kumipuyo test.

--- src/core/kumipuyo_test.cc
+++ src/core/kumipuyo_test.cc
@@ -2,3 +2,20 @@
 
 #include <gtest/gtest.h>
 
+TEST(KumipuyoTest, ctor)
+{
+    Kumipuyo k;
+    EXPECT_EQ(PuyoColor::EMPTY, k.axis);
+    EXPECT_EQ(PuyoColor::EMPTY, k.child);
+
+    k = Kumipuyo(PuyoColor::RED, PuyoColor::BLUE);
+    EXPECT_EQ(PuyoColor::RED, k.axis);
+    EXPECT_EQ(PuyoColor::BLUE, k.child);
+}
+
+TEST(KumipuyoTest, equal)
+{
+    EXPECT_EQ(Kumipuyo(PuyoColor::RED, PuyoColor::BLUE), Kumipuyo(PuyoColor::RED, PuyoColor::BLUE));
+    EXPECT_NE(Kumipuyo(PuyoColor::RED, PuyoColor::BLUE), Kumipuyo(PuyoColor::RED, PuyoColor::RED));
+    EXPECT_NE(Kumipuyo(PuyoColor::RED, PuyoColor::BLUE), Kumipuyo(PuyoColor::BLUE, PuyoColor::BLUE));
+}
