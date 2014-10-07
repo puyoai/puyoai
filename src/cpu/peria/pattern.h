cd8a43cfc254dd66806510fe87ea2f61980043da
Hitoshi Yoshida <github@peria.jp>
2014-10-08 01:32:42 +0900

    Decline same colors for neighbor cells.

--- src/cpu/peria/pattern.h
+++ src/cpu/peria/pattern.h
@@ -10,6 +10,8 @@
 namespace peria {
 
 class Pattern {
+  typedef std::pair<char, char> Neighbor;
+  
  public:
   static void ReadBook(std::istream& is);
   static const std::vector<Pattern>& GetAllPattern();
@@ -19,14 +21,18 @@ class Pattern {
   int score() const { return score_; }
 
  private:
+  void Optimize();
   void AppendField(std::string line);
   int GetScore(std::map<char, std::map<PuyoColor, int> >& matching) const;
 
   std::string name_;
   std::deque<std::string> pattern_;
+  std::set<Neighbor> neighbors_;
   int score_;
+
   // Maxium number of Puyos to apply the pattern.
   int max_puyos_;
+
   // The number of Puyos to check in the pattern.
   int num_puyos_ = 0;
 };
