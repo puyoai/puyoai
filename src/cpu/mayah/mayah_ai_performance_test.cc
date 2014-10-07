e85f45b27bc30cd872289a213ed71ad162ab28bd
MAYAH <mayah@mayah.jp>
2014-10-08 01:37:15 +0900

    Add a few more mayah_ai performance tests.

--- src/cpu/mayah/mayah_ai_performance_test.cc
+++ src/cpu/mayah/mayah_ai_performance_test.cc
@@ -1,10 +1,13 @@
 #include <iostream>
+
+#include <gflags/gflags.h>
 #include <gtest/gtest.h>
 
 #include "base/time_stamp_counter.h"
 #include "core/algorithm/plan.h"
 #include "core/algorithm/puyo_possibility.h"
 #include "core/field/core_field.h"
+#include "core/frame_request.h"
 #include "core/kumipuyo_seq.h"
 
 #include "mayah_ai.h"
@@ -16,30 +19,18 @@ unique_ptr<MayahAI> makeAI()
     int argc = 1;
     char arg[] = "mayah";
     char* argv[] = {arg};
-    return unique_ptr<MayahAI>(new MayahAI(argc, argv));
+
+    MayahAI* ai = new MayahAI(argc, argv);
+
+    // TODO(mayah): should call gameWillBegin.
+    FrameRequest req;
+    req.frameId = 1;
+    ai->onGameWillBegin(req);
+
+    return unique_ptr<MayahAI>(ai);
 }
 
-TEST(MayahAIPerformanceTest, depth3_iter1)
-{
-    TsumoPossibility::initialize();
-    TimeStampCounterData tsc;
-
-    unique_ptr<MayahAI> ai(makeAI());
-    int frameId = 1;
-    CoreField cf;
-    KumipuyoSeq kumipuyoSeq("RRGG");
-    int depth = 3;
-    int numIteration = 1;
-
-    for (int i = 0; i < 3; ++i) {
-        ScopedTimeStampCounter stsc(&tsc);
-        (void)ai->thinkPlan(frameId, cf, kumipuyoSeq, AdditionalThoughtInfo(), depth, numIteration);
-    }
-
-    tsc.showStatistics();
-}
-
-TEST(MayahAIPerformanceTest, depth2_iter2)
+TEST(MayahAIPerformanceTest, seq2_depth2_iter2)
 {
     TsumoPossibility::initialize();
     TimeStampCounterData tsc;
@@ -59,7 +50,7 @@ TEST(MayahAIPerformanceTest, depth2_iter2)
     tsc.showStatistics();
 }
 
-TEST(MayahAIPerformanceTest, depth2_iter3)
+TEST(MayahAIPerformanceTest, seq2_depth2_iter3)
 {
     TsumoPossibility::initialize();
     TimeStampCounterData tsc;
@@ -77,5 +68,94 @@ TEST(MayahAIPerformanceTest, depth2_iter3)
     }
 
     tsc.showStatistics();
+}
+
+TEST(MayahAIPerformanceTest, seq2_depth3_iter1)
+{
+    TsumoPossibility::initialize();
+    TimeStampCounterData tsc;
+
+    unique_ptr<MayahAI> ai(makeAI());
+    int frameId = 1;
+    CoreField cf;
+    KumipuyoSeq kumipuyoSeq("RRGG");
+    int depth = 3;
+    int numIteration = 1;
+
+    for (int i = 0; i < 3; ++i) {
+        ScopedTimeStampCounter stsc(&tsc);
+        (void)ai->thinkPlan(frameId, cf, kumipuyoSeq, AdditionalThoughtInfo(), depth, numIteration);
+    }
+
+    tsc.showStatistics();
+}
+
+TEST(MayahAIPerformanceTest, seq3_depth3_iter1)
+{
+    TsumoPossibility::initialize();
+    TimeStampCounterData tsc;
+
+    unique_ptr<MayahAI> ai(makeAI());
+    int frameId = 1;
+    CoreField cf;
+    KumipuyoSeq kumipuyoSeq("RRGGYY");
+    int depth = 3;
+    int numIteration = 1;
+
+    for (int i = 0; i < 3; ++i) {
+        ScopedTimeStampCounter stsc(&tsc);
+        (void)ai->thinkPlan(frameId, cf, kumipuyoSeq, AdditionalThoughtInfo(), depth, numIteration);
+    }
+
+    tsc.showStatistics();
+}
+
+TEST(MayahAIPerformanceTest, seq3_depth3_iter2)
+{
+    TsumoPossibility::initialize();
+    TimeStampCounterData tsc;
+
+    unique_ptr<MayahAI> ai(makeAI());
+    int frameId = 1;
+    CoreField cf;
+    KumipuyoSeq kumipuyoSeq("RRGGYY");
+    int depth = 3;
+    int numIteration = 2;
+
+    for (int i = 0; i < 3; ++i) {
+        ScopedTimeStampCounter stsc(&tsc);
+        (void)ai->thinkPlan(frameId, cf, kumipuyoSeq, AdditionalThoughtInfo(), depth, numIteration);
+    }
+
+    tsc.showStatistics();
+}
+
+TEST(MayahAIPerformanceTest, seq3_depth3_iter3)
+{
+    TsumoPossibility::initialize();
+    TimeStampCounterData tsc;
+
+    unique_ptr<MayahAI> ai(makeAI());
+    int frameId = 1;
+    CoreField cf;
+    KumipuyoSeq kumipuyoSeq("RRGGYY");
+    int depth = 3;
+    int numIteration = 3;
+
+    for (int i = 0; i < 3; ++i) {
+        ScopedTimeStampCounter stsc(&tsc);
+        (void)ai->thinkPlan(frameId, cf, kumipuyoSeq, AdditionalThoughtInfo(), depth, numIteration);
+    }
+
+    tsc.showStatistics();
+}
+
+int main(int argc, char* argv[])
+{
+    google::InitGoogleLogging(argv[0]);
+    testing::InitGoogleTest(&argc, argv);
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    return RUN_ALL_TESTS();
 }
 
