07b96f9b82bcdee80c64932998db48c3fff6f2d3
MAYAH <mayah@mayah.jp>
2014-10-12 14:21:56 +0900

    KeySender should take KeySetSeq.
    Since showing KeySetSeq in multiple lines is ugly, we'd like to show
    it in one line.

--- src/wii/serial_key_sender.h
+++ src/wii/serial_key_sender.h
@@ -12,14 +12,16 @@ public:
     explicit SerialKeySender(const std::string& deviceName);
     virtual ~SerialKeySender();
 
-    virtual void sendWait(int ms);
-
-protected:
-    virtual void sendKeySet(const KeySet&) override;
+    virtual void sendWait(int ms) override;
+    virtual void sendKeySet(const KeySet&, bool forceSend) override;
+    virtual void sendKeySetSeq(const KeySetSeq&) override;
 
 private:
+    void sendKeySetInternal(const KeySet&);
+
     int fd_;
     struct termios original_;
+    KeySet lastSent_;
 };
 
 #endif
