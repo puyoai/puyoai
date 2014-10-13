45bc6cd3da10de165883284507873497a5b4aa05
MAYAH <mayah@mayah.jp>
2014-10-13 15:21:20 +0900

    Remove cpu/mayah/run_for_tweaker, since it's no meaning now.

--- src/cpu/mayah/run_for_tweaker_1p.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-exec ./mayah_cpu --log_max_score=true --feature=feature.txt "$@" 2>> run2.err
--- src/cpu/mayah/run_for_tweaker_2p.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-exec ./mayah_cpu --feature=feature.txt "$@" 2> run.err
