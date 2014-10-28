7cae85c6325b51b89c72eaa89498a65c7d9bd644
MAYAH <mayah@mayah.jp>
2014-10-28 19:29:29 +0900

    Add TsumoPossibilityTest more.

--- src/core/algorithm/puyo_possibility_test.cc
+++ src/core/algorithm/puyo_possibility_test.cc
@@ -52,11 +52,29 @@ TEST(TsumoPossibilityTest, necessaryPuyos)
     EXPECT_EQ(0, TsumoPossibility::necessaryPuyos(PuyoSet(0, 0, 0, 0), 1.0));
     EXPECT_EQ(0, TsumoPossibility::necessaryPuyos(PuyoSet(0, 0, 0, 0), 0.8));
 
+    EXPECT_EQ(1, TsumoPossibility::necessaryPuyos(PuyoSet(1, 0, 0, 0), 0.249));
     EXPECT_EQ(1, TsumoPossibility::necessaryPuyos(PuyoSet(1, 0, 0, 0), 0.25));
+    EXPECT_EQ(2, TsumoPossibility::necessaryPuyos(PuyoSet(1, 0, 0, 0), 0.251));
+
     EXPECT_EQ(1, TsumoPossibility::necessaryPuyos(PuyoSet(0, 1, 0, 0), 0.25));
     EXPECT_EQ(1, TsumoPossibility::necessaryPuyos(PuyoSet(0, 0, 1, 0), 0.25));
     EXPECT_EQ(1, TsumoPossibility::necessaryPuyos(PuyoSet(0, 0, 0, 1), 0.25));
 
     EXPECT_EQ(1, TsumoPossibility::necessaryPuyos(PuyoSet(1, 0, 0, 0), 0.2));
-    EXPECT_EQ(2, TsumoPossibility::necessaryPuyos(PuyoSet(1, 0, 0, 0), 0.3));
+    EXPECT_EQ(4, TsumoPossibility::necessaryPuyos(PuyoSet(2, 0, 0, 0), 0.2));
+    EXPECT_EQ(7, TsumoPossibility::necessaryPuyos(PuyoSet(3, 0, 0, 0), 0.2));
+    EXPECT_EQ(3, TsumoPossibility::necessaryPuyos(PuyoSet(1, 1, 0, 0), 0.2));
+    EXPECT_EQ(7, TsumoPossibility::necessaryPuyos(PuyoSet(2, 2, 0, 0), 0.2));
+
+    EXPECT_EQ(3, TsumoPossibility::necessaryPuyos(PuyoSet(1, 0, 0, 0), 0.5));
+    EXPECT_EQ(7, TsumoPossibility::necessaryPuyos(PuyoSet(2, 0, 0, 0), 0.5));
+    EXPECT_EQ(11, TsumoPossibility::necessaryPuyos(PuyoSet(3, 0, 0, 0), 0.5));
+    EXPECT_EQ(5, TsumoPossibility::necessaryPuyos(PuyoSet(1, 1, 0, 0), 0.5));
+    EXPECT_EQ(10, TsumoPossibility::necessaryPuyos(PuyoSet(2, 2, 0, 0), 0.5));
+
+    EXPECT_EQ(6, TsumoPossibility::necessaryPuyos(PuyoSet(1, 0, 0, 0), 0.8));
+    EXPECT_EQ(11, TsumoPossibility::necessaryPuyos(PuyoSet(2, 0, 0, 0), 0.8));
+    EXPECT_EQ(16, TsumoPossibility::necessaryPuyos(PuyoSet(3, 0, 0, 0), 0.8));
+    EXPECT_EQ(8, TsumoPossibility::necessaryPuyos(PuyoSet(1, 1, 0, 0), 0.8));
+    EXPECT_EQ(14, TsumoPossibility::necessaryPuyos(PuyoSet(2, 2, 0, 0), 0.8));
 }
