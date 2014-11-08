25b7476d714e6f151152b77914a5041dedd88e48
MAYAH <mayah@mayah.jp>
2014-11-08 12:51:28 +0900

    Add auto tweaker.

--- src/cpu/mayah/evaluation_feature.h
+++ src/cpu/mayah/evaluation_feature.h
@@ -5,58 +5,72 @@
 #include <vector>
 
 enum EvaluationFeatureKey {
-#define DEFINE_PARAM(NAME, tweakable) NAME,
-#define DEFINE_SPARSE_PARAM(NAME, numValue, tweakable) /* ignored */
+#define DEFINE_PARAM(NAME, tweakability) NAME,
+#define DEFINE_SPARSE_PARAM(NAME, numValue, tweakability) /* ignored */
 #include "evaluation_feature.tab"
 #undef DEFINE_PARAM
 #undef DEFINE_SPARSE_PARAM
 };
 
 enum EvaluationSparseFeatureKey {
-#define DEFINE_PARAM(NAME, tweakable) /* ignored */
-#define DEFINE_SPARSE_PARAM(NAME, numValue, tweakable) NAME,
+#define DEFINE_PARAM(NAME, tweakability) /* ignored */
+#define DEFINE_SPARSE_PARAM(NAME, numValue, tweakability) NAME,
 #include "evaluation_feature.tab"
 #undef DEFINE_PARAM
 #undef DEFINE_SPARSE_PARAM
 };
 
-enum class Tweakable {
-    TWEAKABLE, NOT_TWEAKABLE, IGNORE, ASCENDING, DESCENDING
+enum class Tweakability {
+    NOT_TWEAKABLE, IGNORE, TWEAKABLE
+};
+
+enum class SparseTweakability {
+    NOT_TWEAKABLE, IGNORE, ASCENDING, DESCENDING
 };
 
 class EvaluationFeature {
 public:
-    EvaluationFeature(EvaluationFeatureKey key, Tweakable tweakable, const char* str)
-        : key_(key), tweakable_(tweakable), str_(str) {}
+    EvaluationFeature(EvaluationFeatureKey key, Tweakability tweakability, const char* str)
+        : key_(key), tweakability_(tweakability), str_(str) {}
 
     static const std::vector<EvaluationFeature>& all();
     static const EvaluationFeature& toFeature(EvaluationFeatureKey key) { return all()[key]; }
 
     EvaluationFeatureKey key() const { return key_; }
+    Tweakability tweakability() const { return tweakability_; }
     const std::string& str() const { return str_; }
+
+    bool tweakable() const { return tweakability_ == Tweakability::TWEAKABLE; }
+    bool shouldIgnore() const { return tweakability_ == Tweakability::IGNORE; }
 
 private:
     EvaluationFeatureKey key_;
-    Tweakable tweakable_;
+    Tweakability tweakability_;
     std::string str_;
 };
 
 class EvaluationSparseFeature {
 public:
-    EvaluationSparseFeature(EvaluationSparseFeatureKey key, size_t size, Tweakable tweakable, const char* str) :
-        key_(key), size_(size), tweakable_(tweakable), str_(str) {}
+    EvaluationSparseFeature(EvaluationSparseFeatureKey key, size_t size, SparseTweakability tweakability, const char* str) :
+        key_(key), size_(size), tweakability_(tweakability), str_(str) {}
 
     static const std::vector<EvaluationSparseFeature>& all();
     static const EvaluationSparseFeature& toFeature(EvaluationSparseFeatureKey key) { return all()[key]; }
 
     EvaluationSparseFeatureKey key() const { return key_; }
     size_t size() const { return size_; }
+    SparseTweakability tweakability() const { return tweakability_; }
     const std::string& str() const { return str_; }
+
+    bool tweakable() const { return tweakability_ == SparseTweakability::ASCENDING || tweakability_ == SparseTweakability::DESCENDING; }
+    bool shouldIgnore() const { return tweakability_ == SparseTweakability::IGNORE; }
+    bool ascending() const { return tweakability_ == SparseTweakability::ASCENDING; }
+    bool descending() const { return tweakability_ == SparseTweakability::DESCENDING; }
 
 private:
     EvaluationSparseFeatureKey key_;
     int size_;
-    Tweakable tweakable_;
+    SparseTweakability tweakability_;
     std::string str_;
 };
 
