6eec736d319f52e5bb08147f68457d345d54cf57
MAYAH <mayah@mayah.jp>
2014-11-19 11:03:12 +0900

    Fix RensaResult constructor and add tests.

--- src/core/field/rensa_result.cc
+++ src/core/field/rensa_result.cc
@@ -26,16 +26,19 @@ RensaTrackResult::RensaTrackResult(const string& s) :
 {
     CHECK(s.size() % 6 == 0) << s.size();
 
-    int height = static_cast<int>(s.size() % 6) + 1;
+    int height = static_cast<int>(s.size() / 6);
     for (size_t i = 0; i < s.size(); ++i) {
-        size_t x = i % 6;
+        size_t x = (i % 6) + 1;
         size_t y = height - (i / 6);
+        if (s[i] == ' ' || s[i] == '.')
+            continue;
+
         if ('0' <= s[i] && s[i] <= '9')
             erasedAt_[x][y] = s[i] - '0';
         else if ('A' <= s[i] && s[i] <= 'F')
-            erasedAt_[x][y] = s[i] - 'A';
+            erasedAt_[x][y] = s[i] - 'A' + 10;
         else if ('a' <= s[i] && s[i] <= 'f')
-            erasedAt_[x][y] = s[i] - 'a';
+            erasedAt_[x][y] = s[i] - 'a' + 10;
         else
             CHECK(false) << s[i];
     }
