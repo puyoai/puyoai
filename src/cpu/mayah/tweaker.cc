43643f4ebe72ed35d096e51ee2185bf4267efa14
MAYAH <mayah@mayah.jp>
2014-11-27 01:28:57 +0900

    Tweak hand_width implementation.

--- src/cpu/mayah/tweaker.cc
+++ src/cpu/mayah/tweaker.cc
@@ -265,9 +265,9 @@ int main(int argc, char* argv[])
         runAutoTweaker(executor.get(), parameter, FLAGS_auto_count);
     } else {
         map<double, RunResult> scoreMap;
-        for (double x = -200; x <= -50; x += 50) {
+        for (double x = 200; x <= 400; x += 50) {
             cout << "current x = " << x << endl;
-            parameter.setValue(HAND_WIDTH_3, 3, x);
+            parameter.setValue(HAND_WIDTH_4, 2, x);
             scoreMap[x] = run(executor.get(), parameter);
         }
         for (const auto& m : scoreMap) {
