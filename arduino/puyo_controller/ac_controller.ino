76f4a87a4df497b9962037882e69e5efce1a6dd1
MAYAH <mayah@mayah.jp>
2014-12-09 10:27:54 +0900

    Add ac_controller in arduino. This is not tested, so not sure it works.

---
+++ arduino/puyo_controller/ac_controller.ino
@@ -0,0 +1,74 @@
+#define KEY_UP           0
+#define KEY_RIGHT        1
+#define KEY_DOWN         2
+#define KEY_LEFT         3
+#define KEY_RIGHT_TURN   4
+#define KEY_LEFT_TURN    5
+#define KEY_START        6
+#define KEY_WAIT         7
+
+// TODO(mayah): Change PIN numbers appropriately.
+#define PIN_UP           6
+#define PIN_RIGHT        7
+#define PIN_DOWN         8
+#define PIN_LEFT         9
+#define PIN_RIGHT_TURN   10
+#define PIN_LEFT_TURN    11
+#define PIN_START        12
+
+#define PIN_LED          13
+
+#define ARRAY_SIZE(xs)   (sizeof(xs) / sizeof(xs[0]))
+
+static int OUTPUT_PINS[] = {
+    PIN_UP, PIN_RIGHT, PIN_DOWN, PIN_LEFT,
+    PIN_RIGHT_TURN, PIN_LEFT_TURN, PIN_START, PIN_LED
+};
+
+void setup()
+{
+	// Set PushButton pins as input, turning pull-up on
+	pinMode(PIN_LED, OUTPUT);
+
+    for (int i = 0; i < ARRAY_SIZE(OUTPUT_PINS); ++i)
+        pinMode(OUTPUT_PINS[i], OUTPUT);
+
+    for (int i = 0; i < ARRAY_SIZE(OUTPUT_PINS); ++i)
+        digitalWrite(OUTPUT_PINS[i], LOW);
+
+    Serial.begin(38400);
+    delay(100);
+}
+
+void loop()
+{
+    if (Serial.available() <= 0)
+        return;
+
+    int x = Serial.read();
+    x &= 0xFF;
+
+    if ((x >> KEY_WAIT) & 1) {
+        int wait = x & 0x7F;
+        digitalWrite(PIN_LED, HIGH);
+        delay(wait);
+        return;
+    }
+
+    int up = ((x >> KEY_UP) & 1) ? HIGH : LOW;
+    int right = ((x >> KEY_RIGHT) & 1) ? HIGH : LOW;
+    int left = ((x >> KEY_LEFT) & 1) ? HIGH : LOW;
+    int down = ((x >> KEY_DOWN) & 1) ? HIGH : LOW;
+    int right_turn = ((x >> KEY_RIGHT_TURN) & 1) ? HIGH : LOW;
+    int left_turn = ((x >> KEY_LEFT_TURN) & 1) ? HIGH : LOW;
+    int start = ((x >> KEY_START) & 1) ? HIGH : LOW;
+
+    digitWrite(PIN_UP, up);
+    digitWrite(PIN_RIGHT, right);
+    digitWrite(PIN_DOWN, down);
+    digitWrite(PIN_LEFT, left);
+    digitWrite(PIN_RIGHT_TURN, right_turn);
+    digitWrite(PIN_LEFT_TURN, left_turn);
+    digitWrite(PIN_START, start);
+    delay(34);
+}
