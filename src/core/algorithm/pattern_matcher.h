a9824e35562289c6924f8bc878faa5fa14c3de96
MAYAH <mayah@mayah.jp>
2014-12-24 18:18:27 +0900

    Extract PatternMatcher from OpeningBook.

---
+++ src/core/algorithm/pattern_matcher.h
@@ -0,0 +1,54 @@
+#ifndef CORE_ALGORITHM_PATTERN_MATCHER_H_
+#define CORE_ALGORITHM_PATTERN_MATCHER_H_
+
+#include "core/puyo_color.h"
+
+class CoreField;
+class PatternField;
+
+struct PatternMatchResult {
+    PatternMatchResult(bool matched, double score, int count, int allowedCount) :
+        matched(matched), score(score), count(count), allowedCount(allowedCount) {}
+
+    friend bool operator==(const PatternMatchResult& lhs, const PatternMatchResult& rhs)
+    {
+        return std::tie(lhs.matched, lhs.score, lhs.count, lhs.allowedCount) == std::tie(rhs.matched, rhs.score, rhs.count, rhs.allowedCount);
+    }
+
+    bool matched;
+    double score;
+    int count;
+    int allowedCount;
+};
+
+class PatternMatcher {
+public:
+    PatternMatcher();
+
+    PatternMatchResult match(const PatternField&, const CoreField&);
+
+private:
+    PuyoColor map(char var) const
+    {
+        DCHECK('A' <= var && var <= 'Z') << var;
+        return map_[var - 'A'];
+    }
+
+    bool isSet(char var) const
+    {
+        DCHECK('A' <= var && var <= 'Z') << var;
+        return map_[var - 'A'] != PuyoColor::WALL;
+    }
+
+    PuyoColor set(char var, PuyoColor pc)
+    {
+        DCHECK('A' <= var && var <= 'Z') << var;
+        return map_[var - 'A'] = pc;
+    }
+
+    bool checkCell(char currentVar, char neighborVar, PuyoColor neighborColor);
+
+    PuyoColor map_[26];
+};
+
+#endif
---
+++ src/core/algorithm/pattern_matcher_test.cc
@@ -0,0 +1,185 @@
+#include "core/algorithm/pattern_matcher.h"
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "core/core_field.h"
+#include "core/algorithm/pattern_field.h"
+
+static PatternMatchResult match(const PatternField& pf, const CoreField& cf)
+{
+    PatternMatcher matcher;
+    return matcher.match(pf, cf);
+}
+
+TEST(PatternMatcherTest, match1)
+{
+    PatternField pf(
+        "AAA...");
+
+    CoreField f0;
+    CoreField f1("RRR   ");
+    CoreField f2("R     ");
+    CoreField f3("R R   ");
+
+    EXPECT_EQ(PatternMatchResult(true, 0, 0, 0), match(pf, f0));
+    EXPECT_EQ(PatternMatchResult(true, 3, 3, 0), match(pf, f1));
+    EXPECT_EQ(PatternMatchResult(true, 1, 1, 0), match(pf, f2));
+    EXPECT_EQ(PatternMatchResult(true, 2, 2, 0), match(pf, f3));
+}
+
+TEST(PatternMatcherTest, match2)
+{
+    PatternField pf(
+        "BAD.C."
+        "BBADDD"
+        "AACCCX");
+
+    CoreField f0;
+
+    CoreField f1(
+        "BYGBRB"
+        "BBYGGG"
+        "YYRRRB");
+
+    CoreField f2(
+        "BYB R "
+        "BBYBBB"
+        "YYRRRG");
+
+    EXPECT_EQ(PatternMatchResult(true, 0, 0, 0), match(pf, f0));
+    EXPECT_EQ(PatternMatchResult(true, 16, 16, 0), match(pf, f1));
+    EXPECT_EQ(PatternMatchResult(true, 16, 16, 0), match(pf, f2));
+}
+
+TEST(PatternMatcherTest, match3)
+{
+    PatternField pf(
+        "b....."
+        "BEExy."
+        "DDEXYy"
+        "BACXYZ"
+        "BBACXY"
+        "AACCXY");
+
+    CoreField f(
+        "R..B.B"
+        "YYBB.B");
+
+    EXPECT_EQ(PatternMatchResult(true, 8, 8, 0), match(pf, f));
+}
+
+TEST(PatternMatcherTest, matchWithStar)
+{
+    PatternField pf(
+        ".***CC"
+        ".AABBB");
+
+    CoreField f0;
+
+    CoreField f1(
+        ".YYYGG"
+        ".RRBBB");
+
+    CoreField f2(
+        ".B...."
+        ".BBYYY"
+        ".RRBBB");
+
+    CoreField f3(
+        ".GGGYY"
+        ".RRBBB");
+
+    EXPECT_EQ(PatternMatchResult(true, 0, 0, 0), match(pf, f0));
+    EXPECT_EQ(PatternMatchResult(true, 7, 7, 0), match(pf, f1));
+    EXPECT_EQ(PatternMatchResult(true, 7, 7, 0), match(pf, f2));
+    EXPECT_EQ(PatternMatchResult(true, 7, 7, 0), match(pf, f3));
+}
+
+TEST(PatternMatcherTest, matchWithAllowing)
+{
+    PatternField pf(
+        "..C..."
+        "AAaBB.");
+
+    CoreField f0;
+
+    CoreField f1(
+        "RRBBB.");
+
+    CoreField f2(
+        "RRRBB.");
+
+    CoreField f3(
+        "R.RBB.");
+
+    CoreField f4(
+        "..R..."
+        "RRYRR.");
+
+    CoreField f5(
+        "RRRRR.");
+
+    EXPECT_EQ(PatternMatchResult(true, 0, 0, 0), match(pf, f0));
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f1));
+    EXPECT_EQ(PatternMatchResult(true, 4, 4, 1), match(pf, f2));
+    EXPECT_EQ(PatternMatchResult(true, 3, 3, 1), match(pf, f3));
+    EXPECT_EQ(PatternMatchResult(true, 5, 5, 0), match(pf, f4));
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f5));
+}
+
+TEST(PatternMatcherTest, unmatch1)
+{
+    PatternField pf(
+        "BAD.C."
+        "BBADDD"
+        "AACCCX");
+
+    CoreField f1(
+        "B     "
+        "BYGBRB"
+        "BBYGGG"
+        "YYRRRB");
+
+    CoreField f2(
+        "    R "
+        "BYB R "
+        "BBYBBB"
+        "YYRRRG");
+
+    CoreField f3(
+        "BYO R "
+        "BBYOOO"
+        "YYRRRY");
+
+    CoreField f4(
+        "BRB R "
+        "BBRBBB"
+        "RRRRRY");
+
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f1));
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f2));
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f3));
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f4));
+}
+
+TEST(PatternMatcherTest, unmatch2)
+{
+    PatternField pf(
+        "..AAA.");
+
+    CoreField f1(" B B  ");
+
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f1));
+}
+
+TEST(PatternMatcherTest, unmatch3)
+{
+    PatternField pf(
+        "AAABBB");
+
+    CoreField f("Y    Y");
+
+    EXPECT_EQ(PatternMatchResult(false, 0, 0, 0), match(pf, f));
+}
