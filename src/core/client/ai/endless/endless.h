a6be50dfb4672e9d28a4d5378d54e34df1c7384b
MAYAH <mayah@mayah.jp>
2014-12-28 00:57:46 +0900

    Break endless puyo if ZENKESHI.

--- src/core/client/ai/endless/endless.h
+++ src/core/client/ai/endless/endless.h
@@ -8,13 +8,19 @@
 struct FrameRequest;
 class KumipuyoSeq;
 
+struct EndlessResult {
+    int hand;
+    int score;
+    int maxRensa;
+    bool zenkeshi;
+};
+
 // Endless implements endless mode. This can be used to check your AI's strength.
 class Endless {
 public:
     explicit Endless(std::unique_ptr<AI> ai);
 
-    // Run the endless game (at most 50 hands), and returns the max rensa score.
-    int run(const KumipuyoSeq&);
+    EndlessResult run(const KumipuyoSeq&);
 
     void setVerbose(bool flag) { verbose_ = flag; }
 
