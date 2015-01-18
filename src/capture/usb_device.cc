d5d96294adc608612be973a1262c121a66e060bb
MAYAH <mayah@mayah.jp>
2015-01-18 20:57:04 +0900

    Style fix for usb_device.cc

--- src/capture/usb_device.cc
+++ src/capture/usb_device.cc
@@ -2,22 +2,20 @@
 
 libusb_device *findDevice(int vendor, int product)
 {
-	struct libusb_device **list;
-	struct libusb_device *dev = NULL;
-	struct libusb_device_descriptor descriptor;
-	struct libusb_device *item;
-	int i;
-	ssize_t count;
-	count = libusb_get_device_list(NULL, &list);
-	for (i = 0; i < count; i++) {
-		item = list[i];
-		libusb_get_device_descriptor(item, &descriptor);
-		if (descriptor.idVendor == vendor && descriptor.idProduct == product) {
-			dev = item;
-		} else {
-			libusb_unref_device(item);
-		}
-	}
-	libusb_free_device_list(list, 0);
-	return dev;
+	libusb_device** list = nullptr;
+	libusb_device* dev = nullptr;
+
+    ssize_t count = libusb_get_device_list(NULL, &list);
+    for (ssize_t i = 0; i < count; i++) {
+        libusb_device* item = list[i];
+        libusb_device_descriptor descriptor;
+        libusb_get_device_descriptor(item, &descriptor);
+        if (descriptor.idVendor == vendor && descriptor.idProduct == product) {
+            dev = item;
+        } else {
+            libusb_unref_device(item);
+        }
+    }
+    libusb_free_device_list(list, 0);
+    return dev;
 }
