bda02e6b6cba0dd6a22d7d13a7661eb4a7871f39
MAYAH <mayah@mayah.jp>
2015-01-18 21:30:22 +0900

    stylefix on usb_device.cc

--- src/capture/usb_device.cc
+++ src/capture/usb_device.cc
@@ -2,8 +2,8 @@
 
 libusb_device *findDevice(int vendor, int product)
 {
-	libusb_device** list = nullptr;
-	libusb_device* dev = nullptr;
+    libusb_device** list = nullptr;
+    libusb_device* dev = nullptr;
 
     ssize_t count = libusb_get_device_list(NULL, &list);
     for (ssize_t i = 0; i < count; i++) {
@@ -16,6 +16,7 @@ libusb_device *findDevice(int vendor, int product)
             libusb_unref_device(item);
         }
     }
+
     libusb_free_device_list(list, 0);
     return dev;
 }
