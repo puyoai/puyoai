ad21db2d301a4424b203ac6f4a319f69d68b168d
MAYAH <mayah@mayah.jp>
2015-02-16 21:44:38 +0900

    Add situation_test for cpu/mayah

---
+++ src/cpu/mayah/mayah_ai_situation_test.cc
@@ -0,0 +1,69 @@
+#include "mayah_ai.h"
+
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "base/file.h"
+#include "base/strings.h"
+#include "core/algorithm/puyo_possibility.h"
+#include "solver/problem.h"
+#include "solver/solver.h"
+
+using namespace std;
+
+static unique_ptr<DebuggableMayahAI> makeAI(Executor* executor = nullptr)
+{
+    int argc = 1;
+    char arg[] = "mayah";
+    char* argv[] = {arg};
+    return unique_ptr<DebuggableMayahAI>(new DebuggableMayahAI(argc, argv, executor));
+}
+
+static vector<std::string> listTestcases()
+{
+    const string problemDir = file::joinPath(SRC_DIR, "cpu", "mayah", "situation_tests");
+
+    vector<string> files;
+    if (!file::listFiles(problemDir, &files)) {
+        return vector<string>();
+    }
+
+    vector<string> testcases;
+    for (const auto& f : files) {
+        if (!strings::isSuffix(f, ".toml"))
+            continue;
+
+        testcases.push_back(file::joinPath(problemDir, f));
+    }
+
+    return testcases;
+}
+
+class MayahAISituationTest : public testing::TestWithParam<string> {};
+
+TEST_P(MayahAISituationTest, ThinkAsExpected) {
+    const string& filename = GetParam();
+    cout << filename << endl;
+
+    Solver solver(makeAI());
+    Problem problem = Problem::readProblem(filename);
+
+    Decision d = solver.solve(problem);
+    EXPECT_TRUE(problem.answers.count(d)) << "  Your decision: " << d.toString() << endl;
+}
+
+INSTANTIATE_TEST_CASE_P(
+    SituationTestCases,
+    MayahAISituationTest,
+    testing::ValuesIn(listTestcases()));
+
+int main(int argc, char* argv[])
+{
+    google::InitGoogleLogging(argv[0]);
+    testing::InitGoogleTest(&argc, argv);
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    TsumoPossibility::initialize();
+
+    return RUN_ALL_TESTS();
+}
