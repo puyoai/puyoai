bf3a78560136702119cdedb09d026f02b6948674
Hitoshi Yoshida <github@peria.jp>
2015-02-22 21:49:26 +0900

    Add a new test for Pattern

--- src/cpu/peria/pattern_test.cc
+++ src/cpu/peria/pattern_test.cc
@@ -3,6 +3,8 @@
 #include "core/core_field.h"
 #include "core/puyo_color.h"
 
+#include <sstream>
+
 #include <gtest/gtest.h>
 
 namespace peria {
@@ -11,12 +13,24 @@ class PatternForTest : public Pattern {
  public:
   using Pattern::AppendField;
   using Pattern::GetScore;
+  using Pattern::ParseBook;
   using Pattern::num_puyos_;
   using Pattern::pattern_;
   using Pattern::score_;
 };
 
 class PatternTest : public testing::Test {};
+
+TEST_F(PatternTest, ReadBook) {
+  const std::string book_str("NAME: Begin1\n" "SCORE: 1000\n" "MAX: 8\n"
+                             ".AA...\n" ".BB...\n");
+  std::istringstream iss(book_str);
+
+  PatternForTest pattern;
+  pattern.ParseBook(iss);
+
+  EXPECT_EQ(1000, pattern.score_);
+}
 
 TEST_F(PatternTest, GetScore) {
   PatternForTest pattern;
