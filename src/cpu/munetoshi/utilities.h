7bfb9ddc670939ce0c469d729160ecc44c26e952
munetoshi <munetoshi.iskw@gmail.com>
2015-02-24 22:29:55 +0900

    [cpu/munetoshi] Extract utility logic to source file.

--- src/cpu/munetoshi/utilities.h
+++ src/cpu/munetoshi/utilities.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <algorithm>
 
 #include "core/field_constant.h"
@@ -22,7 +23,7 @@ public:
                 PUYOS_TO_SCORE_TABLE.begin(),
                 PUYOS_TO_SCORE_TABLE.end(),
                 score);
-        return itr - PUYOS_TO_SCORE_TABLE.begin();
+        return static_cast<int>(itr - PUYOS_TO_SCORE_TABLE.begin());
     }
 
 private:
@@ -34,34 +35,6 @@ private:
     puyo_calc& operator =(puyo_calc&&) = delete;
 
     const static std::array<int, MAX_PUYOS + 1> PUYOS_TO_SCORE_TABLE;
-
-    static inline int score_for_a_chain(int puyo_num, int chains) {
-        return puyo_num * 10
-                * calculateRensaBonusCoef(chainBonus(chains), longBonus(puyo_num), 0);
-    }
-
-    static inline int score_for_puyos(int puyo_num) {
-        if (puyo_num < 4) {
-            return 0;
-        }
-        int score = 0;
-        int chains = 1;
-        for (; puyo_num >= 8; puyo_num -= 4, chains += 1) {
-            score += score_for_a_chain(4, chains);
-        }
-        score += score_for_a_chain(puyo_num, chains);
-        return score;
-    }
-
 };
-
-const std::array<int, munetoshi::MAX_PUYOS + 1> puyo_calc::PUYOS_TO_SCORE_TABLE(
-        []()->std::array<int, MAX_PUYOS + 1> {
-    std::array<int, MAX_PUYOS + 1> scores;
-    for (int puyos = 0; puyos < MAX_PUYOS + 1; ++puyos) {
-        scores[puyos] = score_for_puyos(puyos);
-    }
-    return scores;
-}());
 
 } // namespace munetoshi
