4cd751836ebc8f05ab3e8110e8f8447cae301216
MAYAH <mayah@mayah.jp>
2015-03-02 09:33:08 +0900

    Remove run_advanced. Add run_without_joseki.

--- src/cpu/mayah/run_advanced_v.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-export GLOG_v=1
-exec ./mayah_cpu --use_advanced_next=true --num_threads=3 "$@" 2> run.err
--- src/cpu/mayah/run_v.sh
+++ src/cpu/mayah/run_v.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 export GLOG_v=1
-exec ./mayah_cpu "$@" 2> run.err
+exec ./mayah_cpu --use_advanced_next --num_threads=3 "$@" 2> run.err
