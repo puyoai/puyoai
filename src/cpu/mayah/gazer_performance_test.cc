ae69a62fc42ff239b960ae198d8c53241715108e
MAYAH <mayah@mayah.jp>
2015-03-28 18:31:40 +0900

    Add gazer performance test in cpu/mayah.
    We'd like to improve the gazer. Before that, we'd like to have a performance
    test for the gazer.

---
+++ src/cpu/mayah/gazer_performance_test.cc
@@ -0,0 +1,52 @@
+#include "gazer.h"
+
+#include <iostream>
+
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+
+#include "base/time_stamp_counter.h"
+#include "core/algorithm/plan.h"
+#include "core/algorithm/puyo_possibility.h"
+#include "core/core_field.h"
+#include "core/frame_request.h"
+#include "core/kumipuyo_seq.h"
+
+#include "mayah_ai.h"
+
+using namespace std;
+
+static void runTest(const CoreField& cf, const KumipuyoSeq& seq)
+{
+    Gazer gazer;
+    gazer.initialize(1);
+    gazer.gaze(100, cf, seq);
+}
+
+TEST(GazerPerformanceTest, pattern1)
+{
+    CoreField f(
+        "    RB"
+        " B GGG"
+        "GG YBR"
+        "YG YGR"
+        "GBYBGR"
+        "BBYYBG"
+        "GYBGRG"
+        "GGYGGR"
+        "YYBBBR");
+    KumipuyoSeq seq("RBRGRYYG");
+
+    runTest(f, seq);
+}
+
+int main(int argc, char* argv[])
+{
+    google::InitGoogleLogging(argv[0]);
+    testing::InitGoogleTest(&argc, argv);
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    TsumoPossibility::initialize();
+
+    return RUN_ALL_TESTS();
+}
