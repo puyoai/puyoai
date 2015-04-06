644e7b314246f105122570494dad8b13cf847e86
MAYAH <mayah@mayah.jp>
2015-04-07 02:13:19 +0900

    Fix that interactive didn't show whole score correctly.

--- src/cpu/mayah/score_collector.h
+++ src/cpu/mayah/score_collector.h
@@ -35,6 +35,7 @@ public:
             collectedSimpleScore_.scoreMap[ordinal(mode)] += paramMap_.parameter(mode).score(key, v);
         }
     }
+
     void addScore(EvaluationSparseFeatureKey key, int idx, int n)
     {
         for (const auto& mode : ALL_EVALUATION_MODES) {
@@ -70,26 +71,36 @@ class FeatureScoreCollector {
 public:
     typedef CollectedFeatureScore CollectedScore;
 
-    FeatureScoreCollector(const EvaluationParameterMap& paramMap) : collector_(paramMap) {}
+    FeatureScoreCollector(const EvaluationParameterMap& paramMap) : paramMap_(paramMap) {}
 
-    void setMode(EvaluationMode mode) { collector_.setMode(mode); }
+    void setMode(EvaluationMode mode) {
+        for (const auto& m : ALL_EVALUATION_MODES)
+            collectedCoef_.coefMap[ordinal(m)] = 0.0;
+        collectedCoef_.coefMap[ordinal(mode)] = 1.0;
+    }
 
     void addScore(EvaluationFeatureKey key, double v)
     {
-        collector_.addScore(key, v);
+        for (const auto& mode : ALL_EVALUATION_MODES) {
+            collectedFeatureScore_.collectedSimpleScore.scoreMap[ordinal(mode)] += paramMap_.parameter(mode).score(key, v);
+        }
         collectedFeatureScore_.collectedFeatures[key] += v;
     }
 
     void addScore(EvaluationSparseFeatureKey key, int idx, int n)
     {
-        collector_.addScore(key, idx, n);
+        for (const auto& mode : ALL_EVALUATION_MODES) {
+            collectedFeatureScore_.collectedSimpleScore.scoreMap[ordinal(mode)] += paramMap_.parameter(mode).score(key, idx, n);
+        }
         for (int i = 0; i < n; ++i)
             collectedFeatureScore_.collectedSparseFeatures[key].push_back(idx);
     }
 
     void merge(const CollectedFeatureScore& cfs)
     {
-        collector_.merge(cfs.collectedSimpleScore);
+        for (const auto& mode : ALL_EVALUATION_MODES) {
+            collectedFeatureScore_.collectedSimpleScore.scoreMap[ordinal(mode)] += cfs.collectedSimpleScore.score(mode);
+        }
 
         for (const auto& entry : cfs.collectedFeatures) {
             collectedFeatureScore_.collectedFeatures[entry.first] = entry.second;
@@ -105,17 +116,19 @@ public:
     void setBookName(const std::string& bookName) { collectedFeatureScore_.bookName = bookName; }
     const std::string& bookName() const { return collectedFeatureScore_.bookName; }
 
-    const CollectedCoef& collectedCoef() const { return collector_.collectedCoef(); }
+    const CollectedCoef& collectedCoef() const { return collectedCoef_; }
     const CollectedFeatureScore& collectedScore() const { return collectedFeatureScore_; }
-    const EvaluationParameterMap& evaluationParameterMap() const { return collector_.evaluationParameterMap(); }
+    const EvaluationParameterMap& evaluationParameterMap() const { return paramMap_; }
 
-    void setEstimatedRensaScore(int s) { collector_.setEstimatedRensaScore(s); }
-    int estimatedRensaScore() const { return collector_.estimatedRensaScore(); }
+    void setEstimatedRensaScore(int s) { estimatedRensaScore_ = s; }
+    int estimatedRensaScore() const { return estimatedRensaScore_; }
 
     void setPuyosToComplement(const ColumnPuyoList& cpl) { collectedFeatureScore_.puyosToComplement = cpl; }
 private:
-    SimpleScoreCollector collector_;
+    const EvaluationParameterMap& paramMap_;
+    CollectedCoef collectedCoef_;
     CollectedFeatureScore collectedFeatureScore_;
+    int estimatedRensaScore_ = 0;
 };
 
 #endif
