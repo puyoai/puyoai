021c1ad40b45a1a7a5d66a1ca5a8af7a81e9df13
Hitoshi Yoshida <github@peria.jp>
2015-04-14 22:13:39 +0900

    Add a file to build Release version. It does not stop compile, even if some warnings remain.

---
+++ build/ninja-clang-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rootdir="$(cd $(dirname "$0")/..; pwd)"
+outdir="$rootdir/out/Release"
+srcdir="$rootdir/src"
+
+declare -a cmake_opts
+cmake_opts+=("-GNinja")
+cmake_opts+=("-DCMAKE_C_COMPILER=clang")
+cmake_opts+=("-DCMAKE_CXX_COMPILER=clang++")
+cmake_opts+=("-DCMAKE_BUILD_TYPE=Release")
+
+declare -a cmake_cxx_flags
+cmake_cxx_flags+=("-std=c++11" "-Wall" "-Wextra")
+cmake_cxx_flags+=("-O2")
+
+cmake_opts+=("-DCMAKE_CXX_FLAGS=${cmake_cxx_flags[*]}")
+
+mkdir -p $outdir
+cd $outdir
+cmake "${cmake_opts[@]}" $srcdir
+ninja "$@"
