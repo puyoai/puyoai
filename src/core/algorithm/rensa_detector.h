efa1924473c4c5ee2607db3b8a5e08ad4fc068f7
MAYAH <mayah@mayah.jp>
2015-04-26 16:30:48 +0900

    Refactoring: Extract RensaDetectorStrategy from RensaDetector.

--- src/core/algorithm/rensa_detector.h
+++ src/core/algorithm/rensa_detector.h
@@ -3,6 +3,7 @@
 
 #include <functional>
 
+#include "core/algorithm/rensa_detector_strategy.h"
 #include "core/field_constant.h"
 
 class ColumnPuyoList;
@@ -11,39 +12,6 @@ class RensaChainTrackResult;
 class RensaCoefResult;
 class RensaVanishingPositionResult;
 struct RensaResult;
-
-class RensaDetectorStrategy {
-public:
-    enum class Mode {
-        DROP, FLOAT, EXTEND,
-    };
-
-    RensaDetectorStrategy(Mode mode,
-                          int maxNumOfComplementPuyosForKey,
-                          int maxNumOfComplementPuyosForFire,
-                          bool allowsPuttingKeyPuyoOn13thRow) :
-        mode_(mode),
-        maxNumOfComplementPuyosForKey_(maxNumOfComplementPuyosForKey),
-        maxNumOfComplementPuyosForFire_(maxNumOfComplementPuyosForFire),
-        allowsPuttingKeyPuyoOn13thRow_(allowsPuttingKeyPuyoOn13thRow)
-    {
-    }
-
-    static RensaDetectorStrategy defaultFloatStrategy() { return RensaDetectorStrategy(Mode::FLOAT, 3, 3, true); }
-    static RensaDetectorStrategy defaultDropStrategy() { return RensaDetectorStrategy(Mode::DROP, 3, 3, true); }
-    static RensaDetectorStrategy defaultExtendStrategy() { return RensaDetectorStrategy(Mode::EXTEND, 3, 3, false); }
-
-    Mode mode() const { return mode_; }
-    int maxNumOfComplementPuyosForKey() const { return maxNumOfComplementPuyosForKey_; }
-    int maxNumOfComplementPuyosForFire() const { return maxNumOfComplementPuyosForFire_; }
-    bool allowsPuttingKeyPuyoOn13thRow() const { return allowsPuttingKeyPuyoOn13thRow_; }
-
-private:
-    Mode mode_;
-    int maxNumOfComplementPuyosForKey_;
-    int maxNumOfComplementPuyosForFire_;
-    bool allowsPuttingKeyPuyoOn13thRow_;
-};
 
 enum class PurposeForFindingRensa {
     FOR_FIRE,
---
+++ src/core/algorithm/rensa_detector_strategy.h
@@ -0,0 +1,37 @@
+#ifndef CORE_ALGORITHM_RENSA_DETECTOR_STRATEGY_H_
+#define CORE_ALGORITHM_RENSA_DETECTOR_STRATEGY_H_
+
+class RensaDetectorStrategy {
+public:
+    enum class Mode {
+        DROP, FLOAT, EXTEND,
+    };
+
+    RensaDetectorStrategy(Mode mode,
+                          int maxNumOfComplementPuyosForKey,
+                          int maxNumOfComplementPuyosForFire,
+                          bool allowsPuttingKeyPuyoOn13thRow) :
+        mode_(mode),
+        maxNumOfComplementPuyosForKey_(maxNumOfComplementPuyosForKey),
+        maxNumOfComplementPuyosForFire_(maxNumOfComplementPuyosForFire),
+        allowsPuttingKeyPuyoOn13thRow_(allowsPuttingKeyPuyoOn13thRow)
+    {
+    }
+
+    static RensaDetectorStrategy defaultFloatStrategy() { return RensaDetectorStrategy(Mode::FLOAT, 3, 3, true); }
+    static RensaDetectorStrategy defaultDropStrategy() { return RensaDetectorStrategy(Mode::DROP, 3, 3, true); }
+    static RensaDetectorStrategy defaultExtendStrategy() { return RensaDetectorStrategy(Mode::EXTEND, 3, 3, false); }
+
+    Mode mode() const { return mode_; }
+    int maxNumOfComplementPuyosForKey() const { return maxNumOfComplementPuyosForKey_; }
+    int maxNumOfComplementPuyosForFire() const { return maxNumOfComplementPuyosForFire_; }
+    bool allowsPuttingKeyPuyoOn13thRow() const { return allowsPuttingKeyPuyoOn13thRow_; }
+
+private:
+    Mode mode_;
+    int maxNumOfComplementPuyosForKey_;
+    int maxNumOfComplementPuyosForFire_;
+    bool allowsPuttingKeyPuyoOn13thRow_;
+};
+
+#endif
