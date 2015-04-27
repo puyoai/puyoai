54271227c1d4a144de44d1e7a4260644adfca865
MAYAH <mayah@mayah.jp>
2015-04-27 11:24:26 +0900

    Add -march=native

--- build/ninja-clang-release.sh
+++ build/ninja-clang-release.sh
@@ -13,6 +13,7 @@ cmake_opts+=("-DCMAKE_BUILD_TYPE=Release")
 declare -a cmake_cxx_flags
 cmake_cxx_flags+=("-std=c++11" "-Wall" "-Wextra")
 cmake_cxx_flags+=("-O2")
+cmake_cxx_flags+=("-march=native")
 
 cmake_opts+=("-DCMAKE_CXX_FLAGS=${cmake_cxx_flags[*]}")
 
