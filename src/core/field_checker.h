5e71bd4b4a8f48d4e38b9e3ddf8caa59130f737a
MAYAH <mayah@mayah.jp>
2015-04-28 10:40:29 +0900

    Use bit operation for FieldChecker.
    This is a bit faster than the current implementation.

--- src/core/field_checker.h
+++ src/core/field_checker.h
@@ -1,33 +1,45 @@
 #ifndef CORE_FIELD_CHECKER_H_
 #define CORE_FIELD_CHECKER_H_
 
+#include <cstdint>
+
 #include <glog/logging.h>
 
 #include "core/field_constant.h"
 
 // FieldChecker is a bitset whose size is the same as field.
-//
-// For performance reason, this is an array of bool instead of std::bitset or something.
-// Converting a bit to bool is slow.
 class FieldChecker {
 public:
-    FieldChecker() : field_{} {}
+    FieldChecker() : col_{} {}
 
-    bool get(int x, int y) const { return field_[index(x, y)]; }
-    void set(int x, int y, bool flag = true) { field_[index(x, y)] = flag; }
-    void clear(int x, int y) { field_[index(x, y)] = false; }
+    bool get(int x, int y) const
+    {
+        DCHECK(0 <= x && x < FieldConstant::MAP_WIDTH) << x;
+        DCHECK(0 <= y && y < FieldConstant::MAP_HEIGHT) << y;
+        return col_[x] & (1U << y);
+    }
+
+    void set(int x, int y, bool flag = true)
+    {
+        DCHECK(0 <= x && x < FieldConstant::MAP_WIDTH) << x;
+        DCHECK(0 <= y && y < FieldConstant::MAP_HEIGHT) << y;
+        if (flag)
+            col_[x] |= (1U << y);
+        else
+            col_[x] &= ~(1U << y);
+    }
+
+    void clear(int x, int y)
+    {
+        DCHECK(0 <= x && x < FieldConstant::MAP_WIDTH) << x;
+        DCHECK(0 <= y && y < FieldConstant::MAP_HEIGHT) << y;
+        col_[x] &= ~(1U << y);
+    }
 
     bool operator()(int x, int y) const { return get(x, y); }
 
 private:
-    int index(int x, int y) const
-    {
-        DCHECK(0 <= x && x < FieldConstant::MAP_WIDTH);
-        DCHECK(0 <= y && y < FieldConstant::MAP_HEIGHT);
-        return y * FieldConstant::MAP_WIDTH + x;
-    }
-
-    bool field_[FieldConstant::MAP_HEIGHT * FieldConstant::MAP_WIDTH];
+    std::uint16_t col_[8];
 };
 
 #endif // CORE_FIELD_CHECKER_H_
