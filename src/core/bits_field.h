dd388e8a727d0b0720151e34784585e8b296c3e8
MAYAH <mayah@mayah.jp>
2015-05-06 12:02:04 +0900

    Add FieldBits and BitsField.
    This is a first step to implement parallelized logic for field manipulation.

---
+++ src/core/bits_field.h
@@ -0,0 +1,15 @@
+#ifndef CORE_BITS_FIELD_H_
+#define CORE_BITS_FIELD_H_
+
+#include "core/field_bits.h"
+
+// BitsField is a field implementation that uses FieldBits.
+// TODO(mayah): Implement this.
+class BitsField {
+public:
+
+private:
+    FieldBits bits_[8];
+};
+
+#endif
