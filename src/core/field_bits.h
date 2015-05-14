6f3e8611158f5afce5ee156d88b719bd96c009b7
MAYAH <mayah@mayah.jp>
2015-05-15 00:59:21 +0900

    Implements a few FieldBits methods.

--- src/core/field_bits.h
+++ src/core/field_bits.h
@@ -4,22 +4,31 @@
 #include <cstdint>
 
 #include <glog/logging.h>
-#include <emmintrin.h>
+#include <smmintrin.h>
 
 #include "core/field_constant.h"
 
 // FieldBits is a bitset whose size is the same as field.
 class FieldBits {
 public:
-    FieldBits() : c_{} {}
+    FieldBits() : m_(_mm_setzero_si128()) {}
 
     __m128i& xmm() { return m_; }
 
+    bool get(int x, int y) const { return !_mm_testz_si128(s_table_[tableIndex(x, y)], m_); }
+    void set(int x, int y) { m_ = _mm_or_si128(s_table_[tableIndex(x, y)], m_); }
+    void unset(int x, int y) { m_ = _mm_andnot_si128(s_table_[tableIndex(x, y)], m_); }
+
 private:
-    union {
-        std::uint16_t c_[8];
-        __m128i m_;
-    };
+    static __m128i s_table_[128];
+
+    static int tableIndex(int x, int y)
+    {
+        DCHECK(0 <= x && x < 8 && 0 <= y && y < 16) << "x=" << x << " y=" << y;
+        return x * 16 + y;
+    }
+
+    __m128i m_;
 };
 
 #endif // CORE_FIELD_BITS_H_
