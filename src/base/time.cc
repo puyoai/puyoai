235897f73ae868c109bba0b78cdee03b9d398075
MAYAH <mayah@mayah.jp>
2015-05-17 17:03:22 +0900

    Add currentTimeInMillis().

--- src/base/time.cc
+++ src/base/time.cc
@@ -8,3 +8,10 @@ double currentTime()
     gettimeofday(&tv, nullptr);
     return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
+
+std::int64_t currentTimeInMillis()
+{
+    struct timeval tv;
+    gettimeofday(&tv, nullptr);
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
--- src/base/time.h
+++ src/base/time.h
@@ -1,7 +1,11 @@
 #ifndef BASE_TIME_H_
 #define BASE_TIME_H_
 
+#include <cstdint>
+
 // Returns the current time [s].
 double currentTime();
+
+std::int64_t currentTimeInMillis();
 
 #endif
