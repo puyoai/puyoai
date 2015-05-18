ec5f55d86e59bc7d2ebf3049e53546c9709b946a
MAYAH <mayah@mayah.jp>
2015-05-19 00:53:36 +0900

    Add vanish tests more.

--- src/core/bit_field_test.cc
+++ src/core/bit_field_test.cc
@@ -30,7 +30,7 @@ TEST_F(BitFieldTest, ctor)
     }
 }
 
-TEST_F(BitFieldTest, vanish)
+TEST_F(BitFieldTest, vanish1)
 {
     PlainField pf(
         "RRBBBB"
@@ -42,4 +42,57 @@ TEST_F(BitFieldTest, vanish)
 
     EXPECT_EQ(60 * 11, score);
     EXPECT_EQ(FieldBits(pf, PuyoColor::BLUE), erased);
+}
+
+TEST_F(BitFieldTest, vanish2)
+{
+    PlainField pf(
+        "RRBB.B"
+        "RGRRBB");
+    BitField bf(pf);
+
+    FieldBits erased;
+    int score = vanish(&bf, 2, &erased);
+
+    EXPECT_EQ(0, score);
+}
+
+TEST_F(BitFieldTest, vanish3)
+{
+    PlainField pf(
+        "ROOOOR"
+        "OBBBBO"
+        "ROOOOR");
+    BitField bf(pf);
+
+    FieldBits erased;
+    int score = vanish(&bf, 1, &erased);
+
+    EXPECT_EQ(40, score);
+    EXPECT_TRUE(bf.bits(PuyoColor::BLUE).isEmpty());
+    EXPECT_TRUE(bf.bits(PuyoColor::OJAMA).isEmpty());
+}
+
+TEST_F(BitFieldTest, vanish4)
+{
+    PlainField pf(
+        "RR.RRR" // 13
+        "RRRRRR" // 12
+        "OOOOOO"
+        "OOOOOO"
+        "OOOOOO"
+        "OOOOOO" // 8
+        "OOOOOO"
+        "OOOOOO"
+        "OOOOOO"
+        "OOOOOO" // 4
+        "OOOOOO"
+        "OOOOOO"
+        "OOOOOO");
+    BitField bf(pf);
+
+    FieldBits erased;
+    int score = vanish(&bf, 1, &erased);
+
+    EXPECT_EQ(60 * 3, score);
 }
