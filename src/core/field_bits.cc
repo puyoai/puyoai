7a6174dd17914fdc1165a7e493747e989e9f7c9d
MAYAH <mayah@mayah.jp>
2015-05-21 12:10:11 +0900

    Inlining FieldBits::vanishingSeed.
    This makes simulate() really fast.
    
    [ RUN      ] FieldPerformanceTest.simulate_filled
    overhead:
            N = 100000
          min = 28
          max = 3912
      average = 34.192
    deviation = 23.1754
    CoreField:
            N = 100000
          min = 5832
          max = 225734
      average = 6272.86
    deviation = 2177.61
    CoreField (RensaChainTracker):
            N = 100000
          min = 6174
          max = 219798
      average = 6605.45
    deviation = 1595.08
    BitField:
            N = 100000
          min = 2463
          max = 103932
      average = 2713.19
    deviation = 1013.94

--- src/core/field_bits.cc
+++ src/core/field_bits.cc
@@ -4,40 +4,6 @@
 
 using namespace std;
 
-FieldBits FieldBits::vanishingSeed() const
-{
-    //  x
-    // xox              -- o is 3-connected
-    //
-    // xoox  ox   x oo
-    //      xo  xoo oo  -- o is 2-connected.
-    //
-    // So, one 3-connected piece or two 2-connected pieces are necessary and sufficient.
-
-    __m128i u = _mm_and_si128(_mm_slli_epi16(m_, 1), m_);
-    __m128i d = _mm_and_si128(_mm_srli_epi16(m_, 1), m_);
-    __m128i l = _mm_and_si128(_mm_slli_si128(m_, 2), m_);
-    __m128i r = _mm_and_si128(_mm_srli_si128(m_, 2), m_);
-
-    __m128i ud_and = _mm_and_si128(u, d);
-    __m128i lr_and = _mm_and_si128(l, r);
-    __m128i ud_or = _mm_or_si128(u, d);
-    __m128i lr_or = _mm_or_si128(l, r);
-
-    __m128i threes = _mm_or_si128(
-        _mm_and_si128(ud_and, lr_or),
-        _mm_and_si128(lr_and, ud_or));
-
-    __m128i twos = _mm_or_si128(_mm_or_si128(
-        _mm_and_si128(ud_or, lr_or), ud_and), lr_and);
-
-    __m128i two_u = _mm_and_si128(_mm_slli_epi16(twos, 1), twos);
-    __m128i two_l = _mm_and_si128(_mm_slli_si128(twos, 2), twos);
-    __m128i two_twos = _mm_or_si128(two_u, two_l);
-
-    return FieldBits(_mm_or_si128(threes, two_twos));
-}
-
 std::string FieldBits::toString() const
 {
     stringstream ss;
