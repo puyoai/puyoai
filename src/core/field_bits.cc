81117416c227bec223466c561210aab1f96e650a
MAYAH <mayah@mayah.jp>
2015-05-28 01:55:43 +0900

    bit_field.h and field_bits.h should not depend on plain_field.h
    OK to forward declaration, though.

--- src/core/field_bits.cc
+++ src/core/field_bits.cc
@@ -2,7 +2,30 @@
 
 #include <sstream>
 
+#include "core/plain_field.h"
+
 using namespace std;
+
+FieldBits::FieldBits(const PlainField& pf, PuyoColor c)
+{
+    __m128i mask = _mm_set1_epi8(static_cast<char>(c));
+
+    // TODO(mayah): should we use _mm_set_epi16? Which is faster?
+
+    union {
+        __m128i m;
+        std::int16_t s[8];
+    } xmm;
+
+    xmm.s[0] = 0;
+    for (int i = 1; i <= 6; ++i) {
+        __m128i x = _mm_load_si128(reinterpret_cast<const __m128i*>(pf.column(i)));
+        xmm.s[i] = _mm_movemask_epi8(_mm_cmpeq_epi8(x, mask));
+    }
+    xmm.s[7] = 0;
+
+    m_ = xmm.m;
+}
 
 std::string FieldBits::toString() const
 {
