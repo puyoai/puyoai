90b3e45e31067027b26d13adbd2f81389dd3b80a
MAYAH <mayah@mayah.jp>
2015-05-27 20:35:55 +0900

    Extract PatternMatchResult from pattern_matcher.h

--- src/core/algorithm/pattern_matcher.h
+++ src/core/algorithm/pattern_matcher.h
@@ -7,35 +7,13 @@
 #include <tuple>
 #include <vector>
 
-#include "base/small_int_set.h"
+#include "core/algorithm/field_pattern.h"
+#include "core/algorithm/pattern_match_result.h"
 #include "core/column_puyo_list.h"
 #include "core/core_field.h"
 #include "core/puyo_color.h"
-#include "core/algorithm/field_pattern.h"
 
 class CoreField;
-
-struct PatternMatchResult {
-    PatternMatchResult() : matched(false), score(0), count(0), allowedCount(0) {}
-    PatternMatchResult(bool matched, double score, int count, int allowedCount) :
-        matched(matched), score(score), count(count), allowedCount(allowedCount) {}
-    PatternMatchResult(bool matched, double score, int count, int allowedCount,
-                       SmallIntSet unusedVariables) :
-        matched(matched), score(score), count(count), allowedCount(allowedCount),
-        unusedVariables(unusedVariables) {}
-
-    friend bool operator==(const PatternMatchResult& lhs, const PatternMatchResult& rhs)
-    {
-        return std::tie(lhs.matched, lhs.score, lhs.count, lhs.allowedCount, lhs.unusedVariables) ==
-            std::tie(rhs.matched, rhs.score, rhs.count, rhs.allowedCount, rhs.unusedVariables);
-    }
-
-    bool matched;
-    double score;
-    int count;
-    int allowedCount;
-    SmallIntSet unusedVariables; // 'A' -> 0, 'B' -> 1, ...
-};
 
 struct ComplementResult {
     explicit ComplementResult(bool success, int filled = 0) :
