d2c0f933f59d47d0729527cb3825426be98190a6
Katsuki Ohto <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2015-05-29 01:02:22 +0900

    made yuricat-AI.

---
+++ src/cpu/yuricat/main.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+#include "base/base.h"
+#include "core/algorithm/plan.h"
+#include "core/client/ai/ai.h"
+#include "core/core_field.h"
+#include "core/frame_request.h"
+
+class SampleAI : public AI {
+public:
+    SampleAI(int argc, char* argv[]) : AI(argc, argv, "sample") {}
+    ~SampleAI() override {}
+
+    DropDecision think(int frameId, const CoreField& f, const KumipuyoSeq& seq,
+                       const PlayerState& me, const PlayerState& enemy, bool fast) const override
+    {
+        UNUSED_VARIABLE(frameId);
+        UNUSED_VARIABLE(me);
+        UNUSED_VARIABLE(enemy);
+        UNUSED_VARIABLE(fast);
+
+        LOG(INFO) << f.toDebugString() << seq.toString();
+
+        Decision best;
+        int score = -1;
+
+        Plan::iterateAvailablePlans(f, seq, 2, [&best, &score](const RefPlan& plan) {
+                int s = -9999;
+                if (plan.isRensaPlan()) {
+                    s += plan.rensaResult().chains * 10;
+                    s -= plan.decisions().size();
+                }
+
+                if (score > s) {
+					
+                    score = s;
+                    best = plan.decisions().front();
+                }
+        });
+
+		//std::cerr<<"oh"<<std::endl;
+        return DropDecision(best);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
+    google::InstallFailureSignalHandler();
+
+    SampleAI(argc, argv).runLoop();
+    return 0;
+}
