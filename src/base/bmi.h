180cb2a822547528ba7b9089dabdff186b667f6f
MAYAH <mayah@mayah.jp>
2015-05-29 18:52:38 +0900

    Add bmi::extractBits and bmi::depositBits.

---
+++ src/base/bmi.h
@@ -0,0 +1,53 @@
+#ifndef BASE_BMI_H_
+
+#include <cstdint>
+
+#ifdef __BMI2__
+#include <bmi2intrin.h>
+#endif
+
+namespace bmi {
+
+// Same as PEXT instruction.
+//      x: HGEFDCBA
+//   mask: 01100100
+// result: 00000GEC
+inline
+std::uint64_t extractBits(std::uint64_t x, std::uint64_t mask)
+{
+#ifdef __BMI2__
+    return _pext_u64(x, mask);
+#else
+    std::uint64_t res = 0;
+    for (std::uint64_t bb = 1; mask != 0; bb <<= 1) {
+        if (x & mask & -mask)
+            res |= bb;
+        mask &= (mask - 1);
+    }
+    return res;
+#endif
+}
+
+// Same as PDEP instruction.
+//      x: HGFEDCBA
+//   mask: 01100100
+// result: 0CB00A00
+inline
+std::uint64_t depositBits(std::uint64_t x, std::uint64_t mask)
+{
+#ifdef __BMI2__
+    return _pdep_u64(x, mask);
+#else
+    std::uint64_t res = 0;
+    for (std::uint64_t bb = 1; mask != 0; bb <<= 1) {
+        if (x & bb)
+            res |= mask & (-mask);
+            mask &= (mask - 1);
+    }
+    return res;
+#endif
+}
+
+} // namespace bmi
+
+#endif
