d4fbccb3c6c9ae53ca431bfa86261c24ac3c7267
MAYAH <mayah@mayah.jp>
2015-06-10 19:43:55 +0900

    Add RensaDetectorPerformanceTest::detectIteratively_Extend.

--- src/core/algorithm/rensa_detector_performance_test.cc
+++ src/core/algorithm/rensa_detector_performance_test.cc
@@ -57,3 +57,25 @@ TEST(RensaDetectorPerformanceTest, detectIteratively_Float)
 
     tsc.showStatistics();
 }
+
+TEST(RensaDetectorPerformanceTest, detectIteratively_Extend)
+{
+    TimeStampCounterData tsc;
+
+    const CoreField original(
+        "  R G "
+        "R GRBG"
+        "RBGRBG"
+        "RBGRBG");
+
+    auto callback = [&](CoreField&& cf, const ColumnPuyoList&) -> RensaResult {
+        return cf.simulate();
+    };
+
+    for (int i = 0; i < 10000; ++i) {
+        ScopedTimeStampCounter stsc(&tsc);
+        RensaDetector::detectIteratively(original, RensaDetectorStrategy::defaultExtendStrategy(), 3, callback);
+    }
+
+    tsc.showStatistics();
+}
