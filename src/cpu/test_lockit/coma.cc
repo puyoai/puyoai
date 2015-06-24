95406195d1dbddcabd9a8de01c13da322a4b10b8
MAYAH <mayah@mayah.jp>
2015-06-24 22:09:43 +0900

    Fix setti_puyo.
    setti_puyo does not update setti_basyo when there is no place to
    put puyos. However, this causes SIGSEGV in chousei_syoukyo etc.
    
    BUG=#72

--- src/cpu/test_lockit/coma.cc
+++ src/cpu/test_lockit/coma.cc
@@ -9,6 +9,8 @@
 #include <sstream>
 #include <string>
 
+#include <glog/logging.h>
+
 #include "color.h"
 #include "field.h"
 
@@ -1570,69 +1572,100 @@ int COMAI_HI::tobashi_hantei_a(const int ba2[][kHeight], int aa, int nx1, int nx
     return 0;
 }
 
-int COMAI_HI::setti_puyo(int ba[][kHeight], int aa, int nx1, int nx2, int setti_basyo[])
+bool COMAI_HI::setti_puyo(int ba[][kHeight], int aa, int nx1, int nx2, int setti_basyo[])
 {
-    int j;
     if (aa < 6) {
-        for (j = 0; j < 13; j++) {
-            if (ba[aa][j] == 0) {
+        setti_basyo[0] = aa;
+        setti_basyo[1] = -1;
+        setti_basyo[2] = aa;
+        setti_basyo[3] = -1;
+        for (int j = 0; j < 13; j++) {
+            if (ba[aa][j] == TL_EMPTY) {
                 ba[aa][j] = nx1;
                 ba[aa][j + 1] = nx2;
-                setti_basyo[0] = aa;
                 setti_basyo[1] = j;
-                setti_basyo[2] = aa;
                 setti_basyo[3] = j + 1;
-                break;
+                return true;
             }
         }
-    } else if (aa < 12) {
-        for (j = 0; j < 13; j++) {
-            if (ba[aa - 6][j] == 0) {
+
+        return false;
+    }
+
+    if (aa < 12) {
+        setti_basyo[0] = aa - 6;
+        setti_basyo[1] = -1;
+        setti_basyo[2] = aa - 6;
+        setti_basyo[3] = -1;
+        for (int j = 0; j < 13; j++) {
+            if (ba[aa - 6][j] == TL_EMPTY) {
                 ba[aa - 6][j] = nx2;
                 ba[aa - 6][j + 1] = nx1;
-                setti_basyo[0] = aa - 6;
                 setti_basyo[1] = j;
-                setti_basyo[2] = aa - 6;
                 setti_basyo[3] = j + 1;
-                break;
+                return true;
             }
         }
-    } else if (aa < 17) {
-        for (j = 0; j < 13; j++) {
-            if (ba[aa - 11][j] == 0) {
-                ba[aa - 11][j] = nx1;
-                setti_basyo[0] = aa - 11;
-                setti_basyo[1] = j;
-                break;
-            }
-        }
-        for (j = 0; j < 13; j++) {
-            if (ba[aa - 12][j] == 0) {
-                ba[aa - 12][j] = nx2;
-                setti_basyo[2] = aa - 12;
-                setti_basyo[3] = j;
-                break;
-            }
-        }
-    } else if (aa < 22) {
-        for (j = 0; j < 13; j++) {
-            if (ba[aa - 17][j] == 0) {
-                ba[aa - 17][j] = nx1;
-                setti_basyo[0] = aa - 17;
-                setti_basyo[1] = j;
-                break;
-            }
-        }
-        for (j = 0; j < 13; j++) {
-            if (ba[aa - 16][j] == 0) {
-                ba[aa - 16][j] = nx2;
-                setti_basyo[2] = aa - 16;
-                setti_basyo[3] = j;
-                break;
-            }
-        }
+
+        return false;
     }
-    return 0;
+
+    if (aa < 17) {
+        setti_basyo[0] = aa - 11;
+        setti_basyo[1] = -1;
+        setti_basyo[2] = aa - 12;
+        setti_basyo[3] = -1;
+        for (int j = 0; j < 13; j++) {
+            if (ba[aa - 11][j] == TL_EMPTY) {
+                setti_basyo[1] = j;
+                break;
+            }
+        }
+        for (int j = 0; j < 13; j++) {
+            if (ba[aa - 12][j] == TL_EMPTY) {
+                setti_basyo[3] = j;
+                break;
+            }
+        }
+
+        if (setti_basyo[1] >= 0 && setti_basyo[3] >= 0) {
+            ba[aa - 11][setti_basyo[1]] = nx1;
+            ba[aa - 12][setti_basyo[3]] = nx2;
+            return true;
+        }
+
+        return false;
+    }
+
+    if (aa < 22) {
+        setti_basyo[0] = aa - 17;
+        setti_basyo[1] = -1;
+        setti_basyo[2] = aa - 16;
+        setti_basyo[3] = -1;
+        for (int j = 0; j < 13; j++) {
+            if (ba[aa - 17][j] == TL_EMPTY) {
+                setti_basyo[1] = j;
+                break;
+            }
+        }
+        for (int j = 0; j < 13; j++) {
+            if (ba[aa - 16][j] == TL_EMPTY) {
+                setti_basyo[3] = j;
+                break;
+            }
+        }
+
+        if (setti_basyo[1] >= 0 && setti_basyo[3] >= 0) {
+            ba[aa - 17][setti_basyo[1]] = nx1;
+            ba[aa - 16][setti_basyo[3]] = nx2;
+            return true;
+        }
+
+        return false;
+    }
+
+    CHECK(false) << "Invalid aa=" << aa;
+    return false;
 }
 
 int COMAI_HI::tobashi_hantei_b(const int ba2[][kHeight], int aa)
