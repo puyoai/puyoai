dd009f1d18caf1774676d238583b4ad078d09fbe
MAYAH <mayah@mayah.jp>
2015-07-04 00:05:28 +0900

    Fix niina crashing after the game.
    The previous fix was invalid.
    This really fixes, but I don't think this is the correct way...
    We need to consider the better way.

--- src/cpu/test_lockit/cpu.cc
+++ src/cpu/test_lockit/cpu.cc
@@ -122,12 +122,30 @@ FrameResponse sendmes(READ_P* p1, COMAI_HI* coo)
     return FrameResponse(p1->id, Decision(p1->te_x, p1->te_r), ss.str());
 }
 
+bool isTsumoValid(int tsumo[6])
+{
+    // HACK(mayah): Check only [2, 4).
+    // [0, 2) might be EMPTY before the game has started.
+    // [4, 6) might be EMPTY before WNEXT come.
+    for (int i = 2; i < 4; ++i) {
+        TLColor c = static_cast<TLColor>(tsumo[i]);
+        if (!isNormalTLColor(c))
+            return false;
+    }
+
+    return true;
+}
+
 FrameResponse TestLockitAI::playOneFrame(const FrameRequest& request)
 {
     int saidaiten, tmp;
     int coma2x_sc[22] {};
 
     parseRequest(request, &r_player[0], &r_player[1], &coma);
+
+    if (!isTsumoValid(r_player[0].tsumo) || !isTsumoValid(r_player[1].tsumo)) {
+        return FrameResponse(request.frameId);
+    }
 
     FrameResponse response(request.frameId);
 
