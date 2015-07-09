2f9c404ad69213c7b1375f1c954488827d016dac
MAYAH <mayah@mayah.jp>
2015-07-09 21:59:55 +0900

    Extract saiki_3 and saiki_4 to field.
    There should not be the methods of COMAI_HI.

--- src/cpu/test_lockit/field.h
+++ src/cpu/test_lockit/field.h
@@ -8,6 +8,8 @@ namespace test_lockit {
 bool IsTLFieldEmpty(const int field[6][kHeight]);
 
 void saiki(const int field[][kHeight], int point[][12], int x, int y, int* num, int incol);
+void saiki_3(const int ba[][kHeight], int point[][12], int x, int y, int* num, int incol);
+void saiki_4(int ba[][kHeight], int x, int y, int* num, int incol);
 
 }  // namespace test_lockit
 
