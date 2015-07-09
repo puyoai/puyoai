9f7f79455c05e0fe81cf3d8747e9ccdb59bcab47
MAYAH <mayah@mayah.jp>
2015-07-09 21:38:18 +0900

    Move saiki_* to anonymous namespace.

--- src/cpu/test_lockit/field.cc
+++ src/cpu/test_lockit/field.cc
@@ -2,6 +2,63 @@
 
 #include "color.h"
 
+namespace {
+
+void saiki_right(const int[][test_lockit::kHeight], int[][12], int, int, int*, int);
+void saiki_left(const int[][test_lockit::kHeight], int[][12], int, int, int*, int);
+void saiki_up(const int[][test_lockit::kHeight], int[][12], int, int, int*, int);
+void saiki_down(const int[][test_lockit::kHeight], int[][12], int, int, int*, int);
+
+void saiki_right(const int ba[][test_lockit::kHeight], int point[][12], int x, int y, int* num, int incol)
+{
+    point[x][y] = 1;
+    *num += 1;
+    if ((y != 11) && (incol == ba[x][y + 1]) && (point[x][y + 1] == 0))
+        saiki_up(ba, point, x, y + 1, num, incol);
+    if ((x != 5) && (incol == ba[x + 1][y]) && (point[x + 1][y] == 0))
+        saiki_right(ba, point, x + 1, y, num, incol);
+    if ((y != 0) && (incol == ba[x][y - 1]) && (point[x][y - 1] == 0))
+        saiki_down(ba, point, x, y - 1, num, incol);
+}
+
+void saiki_left(const int ba[][test_lockit::kHeight], int point[][12], int x, int y, int* num, int incol)
+{
+    point[x][y] = 1;
+    *num += 1;
+    if ((x != 0) && (incol == ba[x - 1][y]) && (point[x - 1][y] == 0))
+        saiki_left(ba, point, x - 1, y, num, incol);
+    if ((y != 11) && (incol == ba[x][y + 1]) && (point[x][y + 1] == 0))
+        saiki_up(ba, point, x, y + 1, num, incol);
+    if ((y != 0) && (incol == ba[x][y - 1]) && (point[x][y - 1] == 0))
+        saiki_down(ba, point, x, y - 1, num, incol);
+}
+
+void saiki_up(const int ba[][test_lockit::kHeight], int point[][12], int x, int y, int* num, int incol)
+{
+    point[x][y] = 1;
+    *num += 1;
+    if ((x != 0) && (incol == ba[x - 1][y]) && (point[x - 1][y] == 0))
+        saiki_left(ba, point, x - 1, y, num, incol);
+    if ((y != 11) && (incol == ba[x][y + 1]) && (point[x][y + 1] == 0))
+        saiki_up(ba, point, x, y + 1, num, incol);
+    if ((x != 5) && (incol == ba[x + 1][y]) && (point[x + 1][y] == 0))
+        saiki_right(ba, point, x + 1, y, num, incol);
+}
+
+void saiki_down(const int ba[][test_lockit::kHeight], int point[][12], int x, int y, int* num, int incol)
+{
+    point[x][y] = 1;
+    *num += 1;
+    if ((x != 0) && (incol == ba[x - 1][y]) && (point[x - 1][y] == 0))
+        saiki_left(ba, point, x - 1, y, num, incol);
+    if ((x != 5) && (incol == ba[x + 1][y]) && (point[x + 1][y] == 0))
+        saiki_right(ba, point, x + 1, y, num, incol);
+    if ((y != 0) && (incol == ba[x][y - 1]) && (point[x][y - 1] == 0))
+        saiki_down(ba, point, x, y - 1, num, incol);
+}
+
+} // namespace anonymous
+
 namespace test_lockit {
 
 bool IsTLFieldEmpty(const int field[6][kHeight])
@@ -22,54 +79,6 @@ void saiki(const int ba[][kHeight], int point[][12], int x, int y, int* num, int
         saiki_left(ba, point, x - 1, y, num, incol);
     if ((y != 11) && (incol == ba[x][y + 1]) && (point[x][y + 1] == 0))
         saiki_up(ba, point, x, y + 1, num, incol);
-    if ((x != 5) && (incol == ba[x + 1][y]) && (point[x + 1][y] == 0))
-        saiki_right(ba, point, x + 1, y, num, incol);
-    if ((y != 0) && (incol == ba[x][y - 1]) && (point[x][y - 1] == 0))
-        saiki_down(ba, point, x, y - 1, num, incol);
-}
-
-void saiki_right(const int ba[][kHeight], int point[][12], int x, int y, int* num, int incol)
-{
-    point[x][y] = 1;
-    *num += 1;
-    if ((y != 11) && (incol == ba[x][y + 1]) && (point[x][y + 1] == 0))
-        saiki_up(ba, point, x, y + 1, num, incol);
-    if ((x != 5) && (incol == ba[x + 1][y]) && (point[x + 1][y] == 0))
-        saiki_right(ba, point, x + 1, y, num, incol);
-    if ((y != 0) && (incol == ba[x][y - 1]) && (point[x][y - 1] == 0))
-        saiki_down(ba, point, x, y - 1, num, incol);
-}
-
-void saiki_left(const int ba[][kHeight], int point[][12], int x, int y, int* num, int incol)
-{
-    point[x][y] = 1;
-    *num += 1;
-    if ((x != 0) && (incol == ba[x - 1][y]) && (point[x - 1][y] == 0))
-        saiki_left(ba, point, x - 1, y, num, incol);
-    if ((y != 11) && (incol == ba[x][y + 1]) && (point[x][y + 1] == 0))
-        saiki_up(ba, point, x, y + 1, num, incol);
-    if ((y != 0) && (incol == ba[x][y - 1]) && (point[x][y - 1] == 0))
-        saiki_down(ba, point, x, y - 1, num, incol);
-}
-
-void saiki_up(const int ba[][kHeight], int point[][12], int x, int y, int* num, int incol)
-{
-    point[x][y] = 1;
-    *num += 1;
-    if ((x != 0) && (incol == ba[x - 1][y]) && (point[x - 1][y] == 0))
-        saiki_left(ba, point, x - 1, y, num, incol);
-    if ((y != 11) && (incol == ba[x][y + 1]) && (point[x][y + 1] == 0))
-        saiki_up(ba, point, x, y + 1, num, incol);
-    if ((x != 5) && (incol == ba[x + 1][y]) && (point[x + 1][y] == 0))
-        saiki_right(ba, point, x + 1, y, num, incol);
-}
-
-void saiki_down(const int ba[][kHeight], int point[][12], int x, int y, int* num, int incol)
-{
-    point[x][y] = 1;
-    *num += 1;
-    if ((x != 0) && (incol == ba[x - 1][y]) && (point[x - 1][y] == 0))
-        saiki_left(ba, point, x - 1, y, num, incol);
     if ((x != 5) && (incol == ba[x + 1][y]) && (point[x + 1][y] == 0))
         saiki_right(ba, point, x + 1, y, num, incol);
     if ((y != 0) && (incol == ba[x][y - 1]) && (point[x][y - 1] == 0))
--- src/cpu/test_lockit/field.h
+++ src/cpu/test_lockit/field.h
@@ -7,12 +7,7 @@ namespace test_lockit {
 
 bool IsTLFieldEmpty(const int field[6][kHeight]);
 
-
 void saiki(const int field[][kHeight], int point[][12], int x, int y, int* num, int incol);
-void saiki_right(const int[][kHeight], int[][12], int, int, int*, int);
-void saiki_left(const int[][kHeight], int[][12], int, int, int*, int);
-void saiki_up(const int[][kHeight], int[][12], int, int, int*, int);
-void saiki_down(const int[][kHeight], int[][12], int, int, int*, int);
 
 }  // namespace test_lockit
 
