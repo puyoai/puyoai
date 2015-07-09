67df3781bec3e6326873fa71f82e8db571b6d08e
MAYAH <mayah@mayah.jp>
2015-07-09 18:58:14 +0900

    Extract COMAI_HI::saiki to field.
    saiki() should not be related to COMAI_HI.

--- src/cpu/test_lockit/field.h
+++ src/cpu/test_lockit/field.h
@@ -7,7 +7,13 @@ namespace test_lockit {
 
 bool IsTLFieldEmpty(const int field[6][kHeight]);
 
+
+void saiki(const int field[][kHeight], int point[][12], int x, int y, int* num, int incol);
+void saiki_right(const int[][kHeight], int[][12], int, int, int*, int);
+void saiki_left(const int[][kHeight], int[][12], int, int, int*, int);
+void saiki_up(const int[][kHeight], int[][12], int, int, int*, int);
+void saiki_down(const int[][kHeight], int[][12], int, int, int*, int);
+
 }  // namespace test_lockit
 
 #endif  // CPU_TEST_LOCKIT_FIELD_H_
-
