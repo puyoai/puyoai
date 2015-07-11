b646e143f773f231f042b36e2545f0f54aea344c
Hitoshi Yoshida <github@peria.jp>
2015-07-11 12:40:31 +0900

    Extract gtr() to a new file template.{cc|h}
    BUG=#102

---
+++ src/cpu/test_lockit/template.h
@@ -0,0 +1,12 @@
+#ifndef CPU_TEST_LOCKIT_TEMPLATE_H_
+#define CPU_TEST_LOCKIT_TEMPLATE_H_
+
+#include "lockit_constant.h"
+
+namespace test_lockit {
+
+int gtr(const int f[][kHeight]);
+
+}  // namespace test_lockit
+
+#endif  // CPU_TEST_LOCKIT_TEMPLATE_H_
