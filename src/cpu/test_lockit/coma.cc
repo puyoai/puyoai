38bbba0612b9588d009b31f9f6b4fcf28c772945
MAYAH <mayah@mayah.jp>
2015-07-11 12:07:31 +0900

    Remove COMAI_HI::read_template.
    We don't have rensa.dat, so read_template does not work at all.

--- src/cpu/test_lockit/coma.cc
+++ src/cpu/test_lockit/coma.cc
@@ -133,7 +133,6 @@ COMAI_HI::COMAI_HI()
     memset(m_jakkin_b, 0, sizeof(m_jakkin_b));
     memset(m_tankinko, 0, sizeof(m_tankinko));
     memset(m_jatako, 0, sizeof(m_jatako));
-    read_template();
 }
 
 COMAI_HI::~COMAI_HI()
@@ -1355,116 +1354,6 @@ int COMAI_HI::setti_ojama(int f[][kHeight], int ojamako)
         }
     }
     return 0;
-}
-
-int COMAI_HI::read_template()
-{
-    int i, j;
-    FILE* fp;
-    char str[256];
-    int cnt = 0;
-    int a[78];
-
-    if ((fp = fopen("rensa.dat", "r")) == NULL) {
-        return 0;
-    }
-    i = 0;
-
-    while (1) {
-        if (cnt == 0) {
-            if ((fscanf(fp, "%d", &m_numg)) == EOF)
-                break;
-            m_colko[i] = m_numg + 1;
-            if (m_numg >= TM_COLNUM)
-                break;
-        } else if (cnt == 1) {
-            for (j = 1; j < m_colko[i]; j++) {
-                if ((fscanf(fp, "%d%*c", &m_colscore[i][j])) == EOF)
-                    break;
-            }
-        } else if (cnt == 2) {
-            if ((fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6],
-                        &a[7], &a[8], &a[9], &a[10], &a[11], &a[12])) == EOF)
-                break;
-        } else if (cnt == 3) {
-            if ((fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", &a[13], &a[14], &a[15], &a[16], &a[17], &a[18],
-                        &a[19], &a[20], &a[21], &a[22], &a[23], &a[24], &a[25])) == EOF)
-                break;
-        } else if (cnt == 4) {
-            if ((fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", &a[26], &a[27], &a[28], &a[29], &a[30], &a[31],
-                        &a[32], &a[33], &a[34], &a[35], &a[36], &a[37], &a[38])) == EOF)
-                break;
-        } else if (cnt == 5) {
-            if ((fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", &a[39], &a[40], &a[41], &a[42], &a[43], &a[44],
-                        &a[45], &a[46], &a[47], &a[48], &a[49], &a[50], &a[51])) == EOF)
-                break;
-        } else if (cnt == 6) {
-            if ((fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", &a[52], &a[53], &a[54], &a[55], &a[56], &a[57],
-                        &a[58], &a[59], &a[60], &a[61], &a[62], &a[63], &a[64])) == EOF)
-                break;
-        } else if (cnt == 7) {
-            if ((fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", &a[65], &a[66], &a[67], &a[68], &a[69], &a[70],
-                        &a[71], &a[72], &a[73], &a[74], &a[75], &a[76], &a[77])) == EOF)
-                break;
-            for (j = 0; j < 78; j++) {
-                if ((a[j] > 0) && (a[j] < TM_COLNUM)) {
-                    m_katax[i][a[j]][m_kko[i][a[j]]] = j / 13;
-                    m_katay[i][a[j]][m_kko[i][a[j]]] = j % 13;
-                    m_kko[i][a[j]]++;
-                } else if (a[j] == -1) {
-                    m_tankinx[i][m_tankinko[i]] = j / 13;
-                    m_tankiny[i][m_tankinko[i]] = j % 13;
-                    m_tankinko[i]++;
-                } else if (a[j] > 100) {
-                    m_jatax[i][a[j] - 101] = j / 13;
-                    m_jatay[i][a[j] - 101] = j % 13;
-                    m_jatako[i]++;
-                }
-            }
-        } else if (cnt == 8) {
-            if ((fscanf(fp, "%d", &m_numg)) == EOF)
-                break;
-            m_kinko[i] = m_numg;
-            if (m_numg > TM_KINPT)
-                break;
-        } else if (cnt == 9) {
-            for (j = 0; j < m_kinko[i]; j++) {
-                if ((fscanf(fp, "%d%*c", &m_kinsi_a[i][j])) == EOF)
-                    break;
-                if ((fscanf(fp, "%d%*c", &m_kinsi_b[i][j])) == EOF)
-                    break;
-            }
-        } else if (cnt == 10) {
-            if ((fscanf(fp, "%d", &m_numg)) == EOF)
-                break;
-            m_jakkinko[i] = m_numg;
-            if (m_numg > TM_JAKKINPT)
-                break;
-        } else if (cnt == 11) {
-            for (j = 0; j < m_jakkinko[i]; j++) {
-                if ((fscanf(fp, "%d%*c", &m_jakkin_a[i][j])) == EOF)
-                    break;
-                if ((fscanf(fp, "%d%*c", &m_jakkin_b[i][j])) == EOF)
-                    break;
-            }
-        } else if (cnt == 12) {
-            if ((fscanf(fp, "%d", &m_numg)) == EOF)
-                break;
-            m_tm_turn[i] = m_numg;
-        } else if (cnt == 13) {
-            if ((fscanf(fp, "%s", str)) == EOF)
-                break;
-            if (strcmp(str, "####################") == 0)
-                i++;
-            else
-                break;
-            cnt = -1;
-        }
-        cnt++;
-    }
-    m_numg = i;
-    fclose(fp);
-    return m_numg;
 }
 
 int COMAI_HI::pre_hyouka(const int ba3[6][kHeight], int tsumo[], int zenkesi_own, int aite_ba[6][kHeight],
--- src/cpu/test_lockit/coma.h
+++ src/cpu/test_lockit/coma.h
@@ -57,8 +57,6 @@ private:
 
     int gtr(const int[][kHeight]);
 
-    int read_template();
-
     int m_cchai;
     int m_conaa;
     int m_nexaa;
@@ -91,21 +89,12 @@ private:
     int m_katax[TM_TMNMUM][TM_COLNUM][TM_COLPER];
     int m_katay[TM_TMNMUM][TM_COLNUM][TM_COLPER];
     int m_kko[TM_TMNMUM][TM_COLNUM];
-    int m_colko[TM_TMNMUM];
-    int m_colscore[TM_TMNMUM][TM_COLNUM];
-    int m_kinko[TM_TMNMUM];
     int m_kinsi_a[TM_TMNMUM][TM_KINPT];
     int m_kinsi_b[TM_TMNMUM][TM_KINPT];
-    int m_jakkinko[TM_TMNMUM];
     int m_jakkin_a[TM_TMNMUM][TM_JAKKINPT];
     int m_jakkin_b[TM_TMNMUM][TM_JAKKINPT];
-    int m_tankinx[TM_TMNMUM][TM_TANKINPT];
-    int m_tankiny[TM_TMNMUM][TM_TANKINPT];
     int m_tankinko[TM_TMNMUM];
-    int m_jatax[TM_TMNMUM][TM_OBJE];
-    int m_jatay[TM_TMNMUM][TM_OBJE];
     int m_jatako[TM_TMNMUM];
-    int m_tm_turn[TM_TMNMUM];
     int m_tm_moni;
     int m_one_tanpatu;
     int m_score_max;
