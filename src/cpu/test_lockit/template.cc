09d0e196126263a22e9245b8be8174fe63833bb8
Hitoshi Yoshida <github@peria.jp>
2015-07-21 22:44:50 +0900

    Remake read_template() as Template class.

--- src/cpu/test_lockit/template.cc
+++ src/cpu/test_lockit/template.cc
@@ -1,5 +1,10 @@
 #include "template.h"
 
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
+
 #include "color.h"
 
 namespace test_lockit {
@@ -43,5 +48,91 @@ int gtr(const TLColor field[][kHeight])
 
     return score;
 }
+
+Template::Template(const std::string& filename)
+{
+    std::ifstream ifs(filename);
+    if (ifs.fail())
+        return;
+
+    int a[78];
+    int i = 0;
+    int cnt = 0;
+    for (std::string line; getline(ifs, line);) {
+        std::replace(line.begin(), line.end(), ',', ' ');
+        std::istringstream iss(line);
+        if (cnt == 0) {
+            if (!(iss >> m_colko[i]))
+                break;
+            ++m_colko[i];
+            if (m_colko[i] > TM_COLNUM)
+                break;
+        } else if (cnt == 1) {
+            for (int j = 1; j < m_colko[i]; j++) {
+                if (!(iss >> m_colscore[i][j]))
+                    break;
+            }
+        } else if (cnt >= 2 && cnt <= 7) {
+            for (int j = (cnt - 2) * 13; j < (cnt - 1) * 13; ++j)
+                if (!(iss >> a[j]))
+                    break;
+
+            if (cnt == 7) {
+                for (int j = 0; j < 78; j++) {
+                    if (a[j] > 0 && a[j] < TM_COLNUM) {
+                        int aj = a[j];
+                        m_katax[i][aj][m_kko[i][aj]] = j / 13;
+                        m_katay[i][aj][m_kko[i][aj]] = j % 13;
+                        m_kko[i][aj]++;
+                    } else if (a[j] == -1) {
+                        m_tankinx[i][m_tankinko[i]] = j / 13;
+                        m_tankiny[i][m_tankinko[i]] = j % 13;
+                        m_tankinko[i]++;
+                    } else if (a[j] > 100) {
+                        m_jatax[i][a[j] - 101] = j / 13;
+                        m_jatay[i][a[j] - 101] = j % 13;
+                        m_jatako[i]++;
+                    }
+                }
+            }
+        } else if (cnt == 8) {
+            if (!(iss >> m_kinko[i]))
+                break;
+            if (m_kinko[i] > TM_KINPT)
+                break;
+        } else if (cnt == 9) {
+            for (int j = 0; j < m_kinko[i]; j++) {
+                if (!(iss >> m_kinsi_a[i][j]))
+                    break;
+                if (!(iss >> m_kinsi_b[i][j]))
+                    break;
+            }
+        } else if (cnt == 10) {
+            if (!(iss >> m_jakkinko[i]))
+                break;
+            if (m_jakkinko[i] > TM_JAKKINPT)
+                break;
+        } else if (cnt == 11) {
+            for (int j = 0; j < m_jakkinko[i]; j++) {
+                if (!(iss >> m_jakkin_a[i][j]))
+                    break;
+                if (!(iss >> m_jakkin_b[i][j]))
+                    break;
+            }
+        } else if (cnt == 12) {
+            if (!(iss >> m_tm_turn[i]))
+                break;
+        } else if (cnt == 13) {
+            if (line.substr(0, 3) != "###")
+                break;
+
+            i++;
+            cnt = -1;
+        }
+        cnt++;
+    }
+    m_numg = i;
+}
+
 
 }  // namespace test_lockit
--- src/cpu/test_lockit/template.h
+++ src/cpu/test_lockit/template.h
@@ -1,12 +1,41 @@
 #ifndef CPU_TEST_LOCKIT_TEMPLATE_H_
 #define CPU_TEST_LOCKIT_TEMPLATE_H_
 
+#include <string>
+
 #include "color.h"
 #include "lockit_constant.h"
 
 namespace test_lockit {
 
 int gtr(const TLColor f[][kHeight]);
+
+class Template {
+public:
+    explicit Template(const std::string& filename);
+
+    // The number of template patterns.
+    int m_numg;
+
+    int m_kko[TM_TMNMUM][TM_COLNUM];
+    int m_colko[TM_TMNMUM];
+    int m_katax[TM_TMNMUM][TM_COLNUM][TM_COLPER];
+    int m_katay[TM_TMNMUM][TM_COLNUM][TM_COLPER];
+    int m_colscore[TM_TMNMUM][TM_COLNUM];
+    int m_kinko[TM_TMNMUM];
+    int m_jakkinko[TM_TMNMUM];
+    int m_tankinx[TM_TMNMUM][TM_TANKINPT];
+    int m_tankiny[TM_TMNMUM][TM_TANKINPT];
+    int m_tankinko[TM_TMNMUM];
+    int m_jatax[TM_TMNMUM][TM_OBJE];
+    int m_jatay[TM_TMNMUM][TM_OBJE];
+    int m_jatako[TM_TMNMUM];
+    int m_kinsi_a[TM_TMNMUM][TM_KINPT];
+    int m_kinsi_b[TM_TMNMUM][TM_KINPT];
+    int m_jakkin_a[TM_TMNMUM][TM_JAKKINPT];
+    int m_jakkin_b[TM_TMNMUM][TM_JAKKINPT];
+    int m_tm_turn[TM_TMNMUM];
+};
 
 }  // namespace test_lockit
 
