0a3370edc3b19f26c793410be6e16af5282f00c8
MAYAH <mayah@mayah.jp>
2015-07-31 18:34:59 +0900

    Extract makePuyopURL from cpu/mayah/tweaker to solver/puyop.h

--- src/cpu/mayah/tweaker.cc
+++ src/cpu/mayah/tweaker.cc
@@ -15,6 +15,7 @@
 #include "core/algorithm/puyo_possibility.h"
 #include "core/sequence_generator.h"
 #include "solver/endless.h"
+#include "solver/puyop.h"
 
 #include "evaluation_parameter.h"
 
@@ -125,61 +126,6 @@ private:
     vector<EvaluationSparseFeature> tweakableSparseFeatures_;
 };
 #endif
-
-string makePuyopURL(const KumipuyoSeq& seq, const vector<Decision>& decisions)
-{
-    static const char ENCODER[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]";
-    stringstream ss;
-
-    ss << "http://www.puyop.com/s/_";
-
-    for (size_t i = 0; i < 50; ++i) {
-        const Kumipuyo& kp = seq.get(i);
-        int d = 0;
-        switch (kp.axis) {
-        case PuyoColor::RED:
-            d += 0;
-            break;
-        case PuyoColor::GREEN:
-            d += 1 * 5;
-            break;
-        case PuyoColor::BLUE:
-            d += 2 * 5;
-            break;
-        case PuyoColor::YELLOW:
-            d += 3 * 5;
-            break;
-        default:
-            CHECK(false);
-        }
-
-        switch (kp.child) {
-        case PuyoColor::RED:
-            d += 0;
-            break;
-        case PuyoColor::GREEN:
-            d += 1;
-            break;
-        case PuyoColor::BLUE:
-            d += 2;
-            break;
-        case PuyoColor::YELLOW:
-            d += 3;
-            break;
-        default:
-            CHECK(false);
-        }
-
-        if (i < decisions.size()) {
-            int h = (decisions[i].x << 2) + decisions[i].r;
-            d |= h << 7;
-        }
-
-        ss << ENCODER[d & 0x3F] << ENCODER[(d >> 6) & 0x3F];
-    }
-
-    return ss.str();
-}
 
 void runOnce(const EvaluationParameterMap& paramMap)
 {
