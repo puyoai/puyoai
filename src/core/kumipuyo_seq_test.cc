3fa0312260a5cf4d3393421bb2ba137f6fc709a5
MAYAH <mayah@mayah.jp>
2015-08-08 12:49:21 +0900

    Fix out of bound accessing in KumipuyoSeq::color.
    Fixed #183
    
    BUG=#183

--- src/core/kumipuyo_seq_test.cc
+++ src/core/kumipuyo_seq_test.cc
@@ -7,3 +7,27 @@ TEST(KumipuyoSeqTest, basic)
     KumipuyoSeq seq;
     EXPECT_EQ(0, seq.size());
 }
+
+TEST(KumipuyoSeqTest, axischild)
+{
+    KumipuyoSeq seq("RBYG");
+
+    EXPECT_EQ(PuyoColor::RED, seq.axis(0));
+    EXPECT_EQ(PuyoColor::BLUE, seq.child(0));
+    EXPECT_EQ(PuyoColor::YELLOW, seq.axis(1));
+    EXPECT_EQ(PuyoColor::GREEN, seq.child(1));
+
+    EXPECT_EQ(2, seq.size());
+}
+
+TEST(KumipuyoSeqTest, color)
+{
+    KumipuyoSeq seq("RBYG");
+
+    EXPECT_EQ(PuyoColor::RED, seq.color(NextPuyoPosition::CURRENT_AXIS));
+    EXPECT_EQ(PuyoColor::BLUE, seq.color(NextPuyoPosition::CURRENT_CHILD));
+    EXPECT_EQ(PuyoColor::YELLOW, seq.color(NextPuyoPosition::NEXT1_AXIS));
+    EXPECT_EQ(PuyoColor::GREEN, seq.color(NextPuyoPosition::NEXT1_CHILD));
+    EXPECT_EQ(PuyoColor::EMPTY, seq.color(NextPuyoPosition::NEXT2_AXIS));
+    EXPECT_EQ(PuyoColor::EMPTY, seq.color(NextPuyoPosition::NEXT2_CHILD));
+}
