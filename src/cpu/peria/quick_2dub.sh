31832d917ba4152049d422fd647b7b95f59f1c22
Hitoshi Yoshida <github@peria.jp>
2015-08-12 23:40:48 +0900

    Add recorder

--- src/cpu/peria/quick_2dub.sh
+++ src/cpu/peria/quick_2dub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-exec ./beam_search_ai --type=2dub "$@" 2> quick_2dub.err
+exec ./beam_search_ai --type=2dub --search_turns=10 --beam_width=200 --v=1 "$@" 2> quick_2dub.err
 
