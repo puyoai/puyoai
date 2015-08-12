23c9a96ec9dd791cc4c0dee42dc6c32c8eba1d00
MAYAH <mayah@mayah.jp>
2015-08-12 13:51:58 +0900

    Fix sse::mm_popcnt_epi16 corner case.

--- src/base/sse_test.cc
+++ src/base/sse_test.cc
@@ -19,16 +19,27 @@ TEST(SSETest, mm_popcnt_epi16)
         __m128i m;
     };
 
-    X m = { { 0x00, 0x10, 0x01, 0x33, 0x77, 0xF0, 0x0F, 0xFF } };
+    X m1 = { { 0x0000, 0x0001, 0x0010, 0x0100, 0x1000, 0x1100, 0x0011, 0x0101 } };
+    X m2 = { { 0x1110, 0x1101, 0x1011, 0x0111, 0xFF00, 0x00FF, 0x0F0F, 0xFFFF } };
 
     X result;
-    result.m = sse::mm_popcnt_epi16(m.m);
+    result.m = sse::mm_popcnt_epi16(m1.m);
     EXPECT_EQ(0, result.v[0]);
     EXPECT_EQ(1, result.v[1]);
     EXPECT_EQ(1, result.v[2]);
-    EXPECT_EQ(4, result.v[3]);
-    EXPECT_EQ(6, result.v[4]);
-    EXPECT_EQ(4, result.v[5]);
-    EXPECT_EQ(4, result.v[6]);
-    EXPECT_EQ(8, result.v[7]);
+    EXPECT_EQ(1, result.v[3]);
+    EXPECT_EQ(1, result.v[4]);
+    EXPECT_EQ(2, result.v[5]);
+    EXPECT_EQ(2, result.v[6]);
+    EXPECT_EQ(2, result.v[7]);
+
+    result.m = sse::mm_popcnt_epi16(m2.m);
+    EXPECT_EQ(3, result.v[0]);
+    EXPECT_EQ(3, result.v[1]);
+    EXPECT_EQ(3, result.v[2]);
+    EXPECT_EQ(3, result.v[3]);
+    EXPECT_EQ(8, result.v[4]);
+    EXPECT_EQ(8, result.v[5]);
+    EXPECT_EQ(8, result.v[6]);
+    EXPECT_EQ(16, result.v[7]);
 }
