b2253219910263d2e0d337be082e9c8d475acd37
MAYAH <mayah@mayah.jp>
2015-11-24 22:41:31 +0900

    Add a few comment in sample.

--- src/cpu/sample/main.cc
+++ src/cpu/sample/main.cc
@@ -12,6 +12,11 @@ public:
     SampleAI(int argc, char* argv[]) : AI(argc, argv, "sample") {}
     ~SampleAI() override {}
 
+    // think() is the method that you need to implement.
+    // frameId: the frame id when the puyo begins to move.
+    // f: current field.
+    // seq; current kumipuyo sequence. It should contain 2 kumipuyos at least.
+    // me, enemy: the detail of each player state.
     DropDecision think(int frameId, const CoreField& f, const KumipuyoSeq& seq,
                        const PlayerState& me, const PlayerState& enemy, bool fast) const override
     {
@@ -25,6 +30,7 @@ public:
         Decision best;
         int score = -1;
 
+        // Do bruteforce search with depth 2. Finds the fireable largest rensa.
         Plan::iterateAvailablePlans(f, seq, 2, [&best, &score](const RefPlan& plan) {
                 int s = 0;
                 if (plan.isRensaPlan()) {
