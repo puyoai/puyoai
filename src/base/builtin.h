26354a6b07de421f40be1d5a92efdd5a70c29059
Hitoshi Yoshida <github@peria.jp>
2016-02-08 00:51:46 +0900

    Support builtin functions in Visual C++

--- src/base/builtin.h
+++ src/base/builtin.h
@@ -6,11 +6,19 @@
 // Returns the number of 1-bits in |x|.
 inline int popCount32(const std::uint32_t& x)
 {
+#if defined(_MSC_VER)
+    return __popcnt(x);
+#else
     return __builtin_popcount(x);
+#endif
 }
 inline int popCount64(const std::uint64_t& x)
 {
+#if defined(_MSC_VER)
+    return __popcnt64(x);
+#else
     return __builtin_popcountll(x);
+#endif
 }
 
 // Returns the number of leading/trailing 0-bits in x, starting at the MSB/LSB.
@@ -19,19 +27,43 @@ inline int popCount64(const std::uint64_t& x)
 //          ^^CLZ counts them.           ^^^^^^CTZ counts them.
 inline int countLeadingZeros32(const std::uint32_t& x)
 {
+#if defined(_MSC_VER)
+    return __lzcnt(x);
+#else
     return __builtin_clz(x);
+#endif
 }
 inline int countTrailingZeros32(const std::uint32_t& x)
 {
+#if defined(_MSC_VER)
+    unsigned long index;
+    if (int ret = _BitScanForward(&index, x)) {
+        return ret;
+    }
+    return 32;
+#else
     return __builtin_ctz(x);
+#endif
 }
 inline int countLeadingZeros64(const std::uint64_t& x)
 {
+#if defined(_MSC_VER)
+    return __lzcnt64(x);
+#else
     return __builtin_clzll(x);
+#endif
 }
 inline int countTrailingZeros64(const std::uint64_t& x)
 {
+#if defined(_MSC_VER)
+    unsigned long index;
+    if (int ret = _BitScanForward64(&index, x)) {
+        return ret;
+    }
+    return 32;
+#else
     return __builtin_ctzll(x);
+#endif
 }
 
 #endif // BASE_BUILTIN_H_
