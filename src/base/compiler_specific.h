4f6dac76ec50041adb5a9c68b4395325faf348f8
MAYAH <mayah@mayah.jp>
2016-02-19 23:59:04 +0900

    Refactoring: Extract compiler specific code to base/compiler_specific.h

---
+++ src/base/compiler_specific.h
@@ -0,0 +1,38 @@
+#ifndef BASE_COMPILER_SPECIFIC_H_
+#define BASE_COMPILER_SPECIFIC_H_
+
+#include "base/build_config.h"
+
+#ifndef __has_feature
+#  define __has_feature(x) 0
+#endif
+
+#ifndef __has_extension
+#  define __has_extension(x) 0
+#endif
+
+#if __has_extension(attribute_deprecated_with_message)
+#  define DEPRECATED_MSG(msg) __attribute__((deprecated(msg)))
+#  define DEPRECATED __attribute__((deprecated()))
+#else
+#  define DEPRECATED_MSG(msg)
+#  define DEPRECATED
+#endif
+
+// NOINLINE_UNLESS_RELEASE is defined in Release build.
+#ifndef NOINLINE_UNLESS_RELEASE
+#ifdef OS_WIN
+#    define NOINLINE_UNLESS_RELEASE __declspec(noinline)
+#else
+#    define NOINLINE_UNLESS_RELEASE __attribute__ ((noinline))
+#endif
+#endif
+
+// CLANG_ALWAYS_INLINE sets __attribute__((always_inline)) only when clang is used.
+#ifdef COMPILER_CLANG
+#define CLANG_ALWAYS_INLINE __attribute__((always_inline))
+#else
+#define CLANG_ALWAYS_INLINE
+#endif
+
+#endif // BASE_COMPILER_SPECIFIC_H_
