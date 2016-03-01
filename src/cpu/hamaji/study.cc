62bff1893cc07c34b20f18a67c73614509f7af97
MAYAH <mayah@mayah.jp>
2016-03-02 01:29:57 +0900

    Fix build failure: cpu/hamaji/study

--- src/cpu/hamaji/study.cc
+++ src/cpu/hamaji/study.cc
@@ -1,17 +1,15 @@
+#include "base/build_config.h"
+
 #include "eval.h"
 
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
 int main(int argc, char* argv[]) {
-#ifdef OS_LINUX
-  InitGoogle(argv[0], &argc, &argv, true);
-#else
   ParseCommandLineFlags(&argc, &argv, true);
   InitGoogleLogging(argv[0]);
-#endif
 
-#ifdef __linux__
+#ifdef OS_LINUX
   FLAGS_logtostderr = true;
 #endif
 
