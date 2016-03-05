04ee8dd930f69ae56e9427015724d3e15cfa68cb
MAYAH <mayah@mayah.jp>
2016-03-06 00:12:25 +0900

    Add base/macros.h
    Add DISALLOW_COPY_AND_ASSIGN.

--- src/base/base.h
+++ src/base/base.h
@@ -1,14 +1,7 @@
 #ifndef BASE_BASE_H_
 #define BASE_BASE_H_
 
-#include <cstddef>
 #include "base/compiler_specific.h"
-
-#define UNUSED_VARIABLE(x) (void)(x)
-
-// C++11 allows us to implement code to take the array size.
-// If we pass non-array to this function, compile error will happen.
-template<typename T, std::size_t size>
-constexpr std::size_t ARRAY_SIZE(const T (&)[size]) { return size; }
+#include "base/macros.h"
 
 #endif  // BASE_BASE_H_
---
+++ src/base/macros.h
@@ -0,0 +1,17 @@
+#ifndef BASE_MACROS_H_
+#define BASE_MACROS_H_
+
+#include <cstddef>
+
+#define UNUSED_VARIABLE(x) (void)(x)
+
+// C++11 allows us to implement code to take the array size.
+// If we pass non-array to this function, compile error will happen.
+template<typename T, std::size_t size>
+constexpr std::size_t ARRAY_SIZE(const T (&)[size]) { return size; }
+
+#define DISALLOW_COPY_AND_ASSIGN(TypeName)     \
+    TypeName(const TypeName&) = delete;        \
+    void operator=(const TypeName&) = delete
+
+#endif // BASE_MACROS_H_
