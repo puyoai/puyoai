5db91ce9741ffd8a2a4f4ee97efa8a851d7cc5cd
mayah <mayah@mayah.jp>
2016-10-15 02:07:41 +0900

    Fix hisya feature more

--- src/cpu/mayah/hisya.sh
+++ src/cpu/mayah/hisya.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-exec ./yukina_cpu --from_wrapper --num_threads=6 \
+exec ./mayah_cpu --from_wrapper --num_threads=3 \
      --feature="cpu/mayah/hisya_feature.toml" \
      --decision_book="cpu/mayah/hisya_decision.toml" \
      --pattern_book="cpu/mayah/hisya_pattern.toml" \
