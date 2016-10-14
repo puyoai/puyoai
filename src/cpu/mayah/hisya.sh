afae6f7ea4e58eb86df402a766a0f373bdf56ca4
mayah <mayah@mayah.jp>
2016-10-15 01:59:35 +0900

    Add hisya feature

--- src/cpu/mayah/hisya.sh
+++ src/cpu/mayah/hisya.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 exec ./yukina_cpu --from_wrapper --num_threads=6 \
+     --feature="cpu/mayah/hisya_feature.toml" \
      --decision_book="cpu/mayah/hisya_decision.toml" \
      --pattern_book="cpu/mayah/hisya_pattern.toml" \
      "$@" 2> run.err
