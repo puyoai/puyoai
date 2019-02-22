e0f79af69d90ca4cde2cd91cece399e89cf56f45
mayah <mayah@mayah.jp>
2019-02-22 10:05:09 +0900

    move cp to build/toolchain

--- build/toolchain/cp.py
+++ build/toolchain/cp.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-#
-# Copyright 2016 Google Inc.
-#
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
 
 import os
 import shutil
@@ -20,4 +15,4 @@ if os.path.exists(dst):
 if os.path.isdir(src):
   shutil.copytree(src, dst)
 else:
-shutil.copy2(src, dst)
+  shutil.copy2(src, dst)
