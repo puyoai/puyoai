4c175daa3ace93be37568e22659d42725a81dc4e
Hitoshi Yoshida <github@peria.jp>
2019-03-09 23:26:31 +0900

    Fix CI configuration

--- build/ci.sh
+++ build/ci.sh
@@ -4,22 +4,6 @@ basedir="$(cd $(dirname "$0")/..; pwd)"
 rootdir="$basedir/puyoai"
 outdir="$rootdir/out/Debug"
 
-export PATH=${rootdir}/depot_tools:${PATH}
-
-gclient config --spec 'solutions = [
-  {
-    "url": "https://github.com/puyoai/puyoai.git",
-    "managed": False,
-    "name": "puyoai",
-    "deps_file": "DEPS",
-    "custom_deps": {
-      "test_resources": None,
-    },
-  },
-]
-'
-gclient sync
-
 # build
 cd $rootdir
 gn gen out/Debug
